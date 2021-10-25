8150ae13f31d9a2e46d9e85383da1c1ede777317
Seiji Okamoto <okamosy@gmail.com>
2021-10-25 14:19:48 +0000

    Add test for get environment by name

--- internal/provider/data_source_environment_test.go
+++ internal/provider/data_source_environment_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccDataSourceEnvironment(t *testing.T) {
+func TestAccDataSourceEnvironment_Id(t *testing.T) {
 
 	var (
 		name         = fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(4))
@@ -21,7 +21,27 @@ func TestAccDataSourceEnvironment(t *testing.T) {
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceEnvironment(name, cac.EnvironmentTypes.Prod),
+				Config: testAccDataSourceEnvironmentById(name, cac.EnvironmentTypes.Prod),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "type", string(cac.EnvironmentTypes.Prod)),
+				),
+			},
+		},
+	})
+}
+func TestAccDataSourceEnvironment_Name(t *testing.T) {
+	var (
+		name         = fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(4))
+		resourceName = "data.harness_environment.test"
+	)
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck: func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceEnvironmentByName(name, cac.EnvironmentTypes.Prod),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "name", name),
 					resource.TestCheckResourceAttr(resourceName, "type", string(cac.EnvironmentTypes.Prod)),
@@ -31,7 +51,7 @@ func TestAccDataSourceEnvironment(t *testing.T) {
 	})
 }
 
-func testAccDataSourceEnvironment(name string, envType cac.EnvironmentType) string {
+func testAccDataSourceEnvironmentById(name string, envType cac.EnvironmentType) string {
 	return fmt.Sprintf(`
 	resource "harness_application" "test" {
 		name = "%[1]s"
@@ -46,6 +66,26 @@ func testAccDataSourceEnvironment(name string, envType cac.EnvironmentType) stri
 	data "harness_environment" "test" {
 		app_id = harness_application.test.id
 		id = harness_environment.test.id
+	}
+
+	`, name, envType)
+}
+
+func testAccDataSourceEnvironmentByName(name string, envType cac.EnvironmentType) string {
+	return fmt.Sprintf(`
+	resource "harness_application" "test" {
+		name = "%[1]s"
+	}
+
+	resource "harness_environment" "test" {
+		app_id = harness_application.test.id
+		name = "%[1]s"
+		type = "%[2]s"
+	}
+
+	data "harness_environment" "test" {
+		app_id = harness_application.test.id
+		name = "%[1]s"
 	}
 
 	`, name, envType)
