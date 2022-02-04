fd6adb69eaae36a2fd47cfafffef4038c1d680b5
Micah Martin <micahlmartin@gmail.com>
2022-02-04 14:43:32 -0500

    Allow attaching permissions to existing user group

--- internal/service/cd/user/user_group.go
+++ internal/service/cd/user/user_group.go
@@ -3,7 +3,6 @@ package user
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	sdk "github.com/harness-io/harness-go-sdk"
 	"github.com/harness-io/harness-go-sdk/harness/cd/graphql"
@@ -11,10 +10,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
-
-var actionsAll = []string{"CREATE", "READ", "UPDATE", "DELETE", "EXECUTE_WORKFLOW", "EXECUTE_PIPELINE", "ROLLBACK_WORKFLOW"}
-var standardActions = []string{"CREATE", "READ", "UPDATE", "DELETE"}
-var deploymentActions = []string{"READ", "EXECUTE_WORKFLOW", "EXECUTE_PIPELINE", "ROLLBACK_WORKFLOW"}
 
 func ResourceUserGroup() *schema.Resource {
 	return &schema.Resource{
@@ -101,263 +96,8 @@ func ResourceUserGroup() *schema.Resource {
 				MaxItems:    1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"account_permissions": {
-							Description: fmt.Sprintf("The account permissions of the user group. Valid options are %s", strings.Join(graphql.AccountPermissionTypeValues, ", ")),
-							Type:        schema.TypeSet,
-							Optional:    true,
-							MinItems:    1,
-							Elem:        &schema.Schema{Type: schema.TypeString},
-						},
-						"app_permissions": {
-							Description: "Application specific permissions",
-							Type:        schema.TypeList,
-							Optional:    true,
-							MaxItems:    1,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"all": {
-										Description: "The permission to perform actions against all resources.",
-										Type:        schema.TypeSet,
-										Optional:    true,
-										Elem: &schema.Resource{
-											Schema: map[string]*schema.Schema{
-												"app_ids": {
-													Description: "The application IDs to which the permission applies. Leave empty to apply to all applications.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"actions": {
-													Description: fmt.Sprintf("The actions allowed to be performed. Valid options are %s", strings.Join(actionsAll, ", ")),
-													Type:        schema.TypeSet,
-													Required:    true,
-													MinItems:    1,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-											},
-										},
-									},
-									"workflow": {
-										Description: "Permission configuration to perform actions against workflows.",
-										Type:        schema.TypeSet,
-										Optional:    true,
-										Elem: &schema.Resource{
-											Schema: map[string]*schema.Schema{
-												"app_ids": {
-													Description: "The application IDs to which the permission applies. Leave empty to apply to all applications.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"filters": {
-													Description: fmt.Sprintf("The filters to apply to the action. Valid options are: %s.", strings.Join(graphql.WorkflowPermissionFiltersSlice, ", ")),
-													Type:        schema.TypeSet,
-													Required:    true,
-													MinItems:    1,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"actions": {
-													Description: fmt.Sprintf("The actions allowed to be performed. Valid options are %s", strings.Join(standardActions, ", ")),
-													Type:        schema.TypeSet,
-													Required:    true,
-													MinItems:    1,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-											},
-										},
-									},
-									"deployment": {
-										Description: "Permission configuration to perform actions against deployments.",
-										Type:        schema.TypeSet,
-										Optional:    true,
-										Elem: &schema.Resource{
-											Schema: map[string]*schema.Schema{
-												"app_ids": {
-													Description: "The application IDs to which the permission applies. Leave empty to apply to all applications.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"env_ids": {
-													Description: "The environment IDs to which the permission applies. Leave empty to apply to all environments.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"filters": {
-													Description: fmt.Sprintf("The filters to apply to the action. Valid options are: %s.", strings.Join(graphql.DeploymentPermissionFiltersSlice, ", ")),
-													Type:        schema.TypeSet,
-													Required:    true,
-													MinItems:    1,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"actions": {
-													Description: fmt.Sprintf("The actions allowed to be performed. Valid options are %s", strings.Join(deploymentActions, ", ")),
-													Type:        schema.TypeSet,
-													Required:    true,
-													MinItems:    1,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-											},
-										},
-									},
-									"environment": {
-										Description: "Permission configuration to perform actions against workflows.",
-										Type:        schema.TypeSet,
-										Optional:    true,
-										Elem: &schema.Resource{
-											Schema: map[string]*schema.Schema{
-												"app_ids": {
-													Description: "The application IDs to which the permission applies. Leave empty to apply to all applications.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"env_ids": {
-													Description: "The environment IDs to which the permission applies. Leave empty to apply to all environments.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"filters": {
-													Description: fmt.Sprintf("The filters to apply to the action. Valid options are: %s.", strings.Join(graphql.EnvFiltersSlice, ", ")),
-													Type:        schema.TypeSet,
-													Required:    true,
-													MinItems:    1,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"actions": {
-													Description: fmt.Sprintf("The actions allowed to be performed. Valid options are %s", strings.Join(standardActions, ", ")),
-													Type:        schema.TypeSet,
-													Required:    true,
-													MinItems:    1,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-											},
-										},
-									},
-									"pipeline": {
-										Description: "Permission configuration to perform actions against pipelines.",
-										Type:        schema.TypeSet,
-										Optional:    true,
-										Elem: &schema.Resource{
-											Schema: map[string]*schema.Schema{
-												"app_ids": {
-													Description: "The application IDs to which the permission applies. Leave empty to apply to all applications.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"env_ids": {
-													Description: "The environment IDs to which the permission applies. Leave empty to apply to all environments.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"filters": {
-													Description: fmt.Sprintf("The filters to apply to the action. Valid options are: %s.", strings.Join(graphql.PipelinePermissionFiltersSlice, ", ")),
-													Type:        schema.TypeSet,
-													Required:    true,
-													MinItems:    1,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"actions": {
-													Description: fmt.Sprintf("The actions allowed to be performed. Valid options are %s", standardActions),
-													Type:        schema.TypeSet,
-													Required:    true,
-													MinItems:    1,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-											},
-										},
-									},
-									"provisioner": {
-										Description: "Permission configuration to perform actions against provisioners.",
-										Type:        schema.TypeSet,
-										Optional:    true,
-										Elem: &schema.Resource{
-											Schema: map[string]*schema.Schema{
-												"app_ids": {
-													Description: "The application IDs to which the permission applies. Leave empty to apply to all applications.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"provisioner_ids": {
-													Description: "The provisioner IDs to which the permission applies. Leave empty to apply to all provisioners.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"actions": {
-													Description: fmt.Sprintf("The actions allowed to be performed. Valid options are %s", strings.Join(standardActions, ", ")),
-													Type:        schema.TypeSet,
-													Required:    true,
-													MinItems:    1,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-											},
-										},
-									},
-									"service": {
-										Description: "Permission configuration to perform actions against services.",
-										Type:        schema.TypeSet,
-										Optional:    true,
-										Elem: &schema.Resource{
-											Schema: map[string]*schema.Schema{
-												"app_ids": {
-													Description: "The application IDs to which the permission applies. Leave empty to apply to all applications.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"service_ids": {
-													Description: "The service IDs to which the permission applies. Leave empty to apply to all services.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"actions": {
-													Description: fmt.Sprintf("The actions allowed to be performed. Valid options are %s", strings.Join(standardActions, ", ")),
-													Type:        schema.TypeSet,
-													Required:    true,
-													MinItems:    1,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-											},
-										},
-									},
-									"template": {
-										Description: "Permission configuration to perform actions against templates.",
-										Type:        schema.TypeSet,
-										Optional:    true,
-										Elem: &schema.Resource{
-											Schema: map[string]*schema.Schema{
-												"app_ids": {
-													Description: "The application IDs to which the permission applies. Leave empty to apply to all applications.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"template_ids": {
-													Description: "The template IDs to which the permission applies. Leave empty to apply to all environments.",
-													Type:        schema.TypeSet,
-													Optional:    true,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-												"actions": {
-													Description: fmt.Sprintf("The actions allowed to be performed. Valid options are %s", strings.Join(standardActions, ", ")),
-													Type:        schema.TypeSet,
-													Required:    true,
-													MinItems:    1,
-													Elem:        &schema.Schema{Type: schema.TypeString},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
+						"account_permissions": getUserGroupAccountPermissionsSchema(),
+						"app_permissions":     getUserGroupAppPermissionsSchema(),
 					},
 				},
 			},
@@ -416,10 +156,6 @@ func ResourceUserGroup() *schema.Resource {
 
 func resourceUserGroupCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	c := meta.(*sdk.Session)
-
-	// if err := validateUserGroupPermissions(d); err != nil {
-	// 	return diag.FromErr(err)
-	// }
 
 	input := &graphql.UserGroup{
 		Name:        d.Get("name").(string),
---
+++ internal/service/cd/user/user_group_permissions.go
@@ -0,0 +1,142 @@
+package user
+
+import (
+	"context"
+	"fmt"
+
+	sdk "github.com/harness-io/harness-go-sdk"
+	"github.com/harness-io/harness-go-sdk/harness/cd/graphql"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func ResourceUserGroupPermissions() *schema.Resource {
+	return &schema.Resource{
+		Description: "Resource for adding permissions to an existing Harness user group.",
+
+		CreateContext: resourceUserGroupPermissionsCreateOrUpdate,
+		ReadContext:   resourceUserGroupPermissionsRead,
+		UpdateContext: resourceUserGroupPermissionsCreateOrUpdate,
+		DeleteContext: resourceUserGroupPermissionsDelete,
+
+		Schema: map[string]*schema.Schema{
+			"user_group_id": {
+				Description: "Unique identifier of the user group.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"account_permissions": getUserGroupAccountPermissionsSchema(),
+			"app_permissions":     getUserGroupAppPermissionsSchema(),
+		},
+
+		Importer: &schema.ResourceImporter{
+			State: func(d *schema.ResourceData, i interface{}) ([]*schema.ResourceData, error) {
+				d.Set("user_group_id", d.Id())
+				return []*schema.ResourceData{d}, nil
+			},
+		},
+	}
+}
+
+func resourceUserGroupPermissionsCreateOrUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*sdk.Session)
+
+	id := d.Get("user_group_id").(string)
+	ug, err := c.CDClient.UserClient.GetUserGroupById(id)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if ug == nil {
+		return diag.FromErr(fmt.Errorf("user group %s does not exist", id))
+	}
+
+	permissions := &graphql.UserGroupPermissions{}
+
+	expandAccountPermissions(d.Get("account_permissions").(*schema.Set).List(), permissions)
+	expandAppPermissions(d.Get("app_permissions").([]interface{}), permissions)
+
+	input := &graphql.UserGroup{
+		Id:          ug.Id,
+		Permissions: permissions,
+	}
+
+	updatedUg, err := c.CDClient.UserClient.UpdateUserGroup(input)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// Computed fields
+	readUserGroupPermissions(d, updatedUg)
+
+	return nil
+}
+
+func resourceUserGroupPermissionsRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*sdk.Session)
+
+	id := d.Get("user_group_id").(string)
+
+	userGroup, err := c.CDClient.UserClient.GetUserGroupById(id)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if userGroup == nil {
+		d.SetId("")
+		d.MarkNewResource()
+		return diag.FromErr(fmt.Errorf("user group %s does not exist", id))
+	}
+
+	return readUserGroupPermissions(d, userGroup)
+}
+
+func readUserGroupPermissions(d *schema.ResourceData, userGroup *graphql.UserGroup) diag.Diagnostics {
+	d.SetId(userGroup.Id)
+
+	if accountPermission := flattenAccountPermissions(userGroup.Permissions); len(accountPermission) > 0 {
+		d.Set("account_permissions", accountPermission)
+	}
+
+	if appPermissions := flattenAppPermissions(userGroup.Permissions); len(appPermissions) > 0 {
+		d.Set("app_permissions", appPermissions)
+	}
+
+	return nil
+}
+
+func resourceUserGroupPermissionsDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*sdk.Session)
+
+	id := d.Id()
+
+	ug, err := c.CDClient.UserClient.GetUserGroupById(id)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if ug == nil {
+		d.SetId("")
+		return nil
+	}
+
+	ug.Permissions.AccountPermissions = &graphql.AccountPermissions{}
+	ug.Permissions.AppPermissions = []*graphql.AppPermission{}
+
+	updatedUg, err := c.CDClient.UserClient.UpdateUserGroup(ug)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if len(updatedUg.Permissions.AccountPermissions.AccountPermissionTypes) > 0 {
+		return diag.FromErr(fmt.Errorf("failed to delete user group permissions"))
+	}
+
+	if len(updatedUg.Permissions.AppPermissions) > 0 {
+		return diag.FromErr(fmt.Errorf("failed to delete user group permissions"))
+	}
+
+	d.SetId("")
+
+	return nil
+}
