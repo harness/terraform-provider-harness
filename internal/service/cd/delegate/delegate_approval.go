67e1f12c288416dc3df4f9e4c76afd1494acc65b
Micah Martin <micahlmartin@gmail.com>
2022-04-18 21:49:27 -0400

    Removed delegate approval update

--- internal/service/cd/delegate/delegate_approval.go
+++ internal/service/cd/delegate/delegate_approval.go
@@ -13,9 +13,8 @@ import (
 func ResourceDelegateApproval() *schema.Resource {
 	return &schema.Resource{
 		Description:   "Resource for approving or rejecting delegates.",
-		CreateContext: resourceDelegateApprovalCreateOrUpdate,
+		CreateContext: resourceDelegateApprovalCreate,
 		ReadContext:   resourceDelegateApprovalRead,
-		UpdateContext: resourceDelegateApprovalCreateOrUpdate,
 		DeleteContext: resourceDelegateApprovalDelete,
 		Schema: map[string]*schema.Schema{
 			"delegate_id": {
@@ -73,13 +72,8 @@ func resourceDelegateApprovalRead(ctx context.Context, d *schema.ResourceData, m
 	return nil
 }
 
-func resourceDelegateApprovalCreateOrUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceDelegateApprovalCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	c := meta.(*internal.Session).CDClient
-
-	// Don't attempt to do anything if we've already done this once.
-	if !d.IsNewResource() {
-		return diag.Errorf("the delegate approval status has already been changed.")
-	}
 
 	id := d.Get("delegate_id").(string)
 	delegate, err := c.DelegateClient.GetDelegateById(id)
