d578d50b9c09aeacf94a365a46b46c977770923d
Micah Martin <micahlmartin@gmail.com>
2022-04-22 06:34:08 -0500

    Yaml config resource should force new resource when path is changed (#117)
    * Force new resource when delegate_id or approve fields have changed
    
    * updated changelog
    
    * Removed delegate approval update
    
    * Set yaml config path to ForceNew
    
    * fixed mods
    
    * Updated changelog

--- internal/service/cd/yamlconfig/yaml_config_test.go
+++ internal/service/cd/yamlconfig/yaml_config_test.go
@@ -13,6 +13,7 @@ import (
 func TestAccResourceYamlConfig_CloudProvider(t *testing.T) {
 
 	expectedName := fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(4))
+	updatedName := fmt.Sprintf("%s_updated", t.Name())
 	resourceName := "harness_yaml_config.test"
 
 	resource.UnitTest(t, resource.TestCase{
@@ -23,6 +24,12 @@ func TestAccResourceYamlConfig_CloudProvider(t *testing.T) {
 				Config: testAccYamlConfigCloudProvider(expectedName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "name", expectedName),
+				),
+			},
+			{
+				Config: testAccYamlConfigCloudProvider(updatedName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", updatedName),
 				),
 			},
 			{
