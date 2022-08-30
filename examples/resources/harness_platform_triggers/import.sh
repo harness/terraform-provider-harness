73fd1a3f18b52a55b65c02108f14896712b596eb
Meet Rathod <rathod.meetsatish@harness.io>
2022-08-30 22:58:29 +0530

    generate documentation

---
+++ examples/resources/harness_platform_triggers/import.sh
@@ -0,0 +1,2 @@
+# Import using trigger id
+terraform import harness_platform_triggers.example <triggers_id>
---
+++ examples/resources/harness_platform_triggers/resource.tf
@@ -0,0 +1,39 @@
+resource "harness_platform_triggers" "example" {
+  identifier = "identifier"
+  org_id     = "orgIdentifer"
+  project_id = "projectIdentifier"
+  name       = "name"
+  target_id  = "pipelineIdentifier"
+  yaml       = <<-EOT
+  ---
+  trigger:
+    name: "name"
+    identifier: "identifier"
+    enabled: true
+    description: ""
+    tags: {}
+    projectIdentifier: "projectIdentifier"
+    orgIdentifier: "orgIdentifer"
+    pipelineIdentifier: "pipelineIdentifier"
+    source:
+      type: "Webhook"
+      spec:
+        type: "Github"
+        spec:
+          type: "Push"
+          spec:
+            connectorRef: "account.TestAccResourceConnectorGithub_Ssh_IZBeG"
+            autoAbortPreviousExecutions: false
+            payloadConditions:
+            - key: "changedFiles"
+              operator: "Equals"
+              value: "value"
+            - key: "targetBranch"
+              operator: "Equals"
+              value: "value"
+            headerConditions: []
+            repoName: "repoName"
+            actions: []
+    inputYaml: "pipeline: {}\n"
+    EOT
+}
