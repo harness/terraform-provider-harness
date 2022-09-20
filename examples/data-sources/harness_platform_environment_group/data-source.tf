31357d2676107ac6b05c89484efbc03b619c6d6b
Rajendra Baviskar <rajendra.baviskar@harness.io>
2022-09-19 23:14:07 -0700

    [SPG-216]: fixing files.

--- examples/data-sources/harness_platform_environment_group/data-source.tf
+++ examples/data-sources/harness_platform_environment_group/data-source.tf
@@ -1,5 +1,5 @@
 data "harness_platform_environment_group" "test" {
-  identifier  = "identifier"
+  identifier = "identifier"
   org_id     = "org_id"
   project_id = "project_id"
 }
--- examples/resources/harness_environment_group/import.sh
+++ examples/resources/harness_environment_group/import.sh
@@ -1,2 +1,2 @@
-# Import using the Harness application id and environment group id.
+# Import using the environment group id.
 terraform import harness_platform_environment_group.example <environment_group_id>
