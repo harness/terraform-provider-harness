ba03517303257ade4441969b0edc3a6d355df480
tchaurasiya <tathagat.chaurasiya@harness.io>
2022-10-05 15:03:27 +0530

    [CDS-39942]: add manifest and configfiles in service

--- internal/service/platform/service/resource_service_test.go
+++ internal/service/platform/service/resource_service_test.go
@@ -138,12 +138,44 @@ func testAccResourceService(id string, name string) string {
 			org_id = harness_platform_project.test.org_id
 			project_id = harness_platform_project.test.id
 			yaml = <<-EOT
-			   service:
-			                 name: "%[1]s"
-			                 identifier: "%[1]s"
-			                 description: "temp"
-			                 orgIdentifier: ${harness_platform_project.test.org_id}
-			                 projectIdentifier: ${harness_platform_project.test.id}
+        service:
+          name: %[1]s
+          identifier: %[2]s
+          serviceDefinition:
+            spec:
+              manifests:
+                - manifest:
+                    identifier: manifest1
+                    type: K8sManifest
+                    spec:
+                      store:
+                        type: Github
+                        spec:
+                          connectorRef: <+input>
+                          gitFetchType: Branch
+                          paths:
+                            - files1
+                          repoName: <+input>
+                          branch: master
+                      skipResourceVersioning: false
+              configFiles:
+                - configFile:
+                    identifier: configFile1
+                    spec:
+                      store:
+                        type: Harness
+                        spec:
+                          files:
+                            - <+org.description>
+              variables:
+                - name: var1
+                  type: String
+                  value: val1
+                - name: var2
+                  type: String
+                  value: val2
+            type: Kubernetes
+          gitOpsEnabled: false
 		  EOT
 		}
 `, id, name)
