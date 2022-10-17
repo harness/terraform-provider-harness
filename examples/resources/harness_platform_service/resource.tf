1501c7c7fa5446114fc7834428c4fe243df94a17
Yuvraj Singhal <yuvraj.singhal@harness.io>
2022-10-17 20:33:14 +0530

    [SPG-427]: Adding example in next gen service documentation

--- examples/resources/harness_platform_service/resource.tf
+++ examples/resources/harness_platform_service/resource.tf
@@ -4,4 +4,44 @@ resource "harness_platform_service" "example" {
   description = "test"
   org_id      = "org_id"
   project_id  = "project_id"
+  yaml        = <<-EOT
+                service:
+                  name: name
+                  identifier: identifier
+                  serviceDefinition:
+                    spec:
+                      manifests:
+                        - manifest:
+                            identifier: manifest1
+                            type: K8sManifest
+                            spec:
+                              store:
+                                type: Github
+                                spec:
+                                  connectorRef: <+input>
+                                  gitFetchType: Branch
+                                  paths:
+                                    - files1
+                                  repoName: <+input>
+                                  branch: master
+                              skipResourceVersioning: false
+                      configFiles:
+                        - configFile:
+                            identifier: configFile1
+                            spec:
+                              store:
+                                type: Harness
+                                spec:
+                                  files:
+                                    - <+org.description>
+                      variables:
+                        - name: var1
+                          type: String
+                          value: val1
+                        - name: var2
+                          type: String
+                          value: val2
+                    type: Kubernetes
+                  gitOpsEnabled: false
+              EOT
 }
