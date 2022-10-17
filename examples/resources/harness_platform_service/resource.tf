7c09a168c2410b09b0495d60e19a3aab4fa5d456
Yuvraj Singhal <yuvraj.singhal@harness.io>
2022-10-17 19:50:45 +0530

    [SPG-427]: Adding example in next gen service documentation

---
+++ examples/resources/harness_platform_service/resource.tf
@@ -0,0 +1,7 @@
+resource "harness_platform_service" "example" {
+  identifier  = "identifier"
+  name        = "name"
+  description = "test"
+  org_id      = "org_id"
+  project_id  = "project_id"
+}
