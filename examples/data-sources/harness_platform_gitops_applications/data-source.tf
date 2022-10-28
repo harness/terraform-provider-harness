a68a945607211e8b84e85f0be9ac9419165f7a9f
Manavjot Singh <manavjot.singh@harness.io>
2022-10-28 09:54:00 -0400

    CDS-44308 adding docs for gitops aagents, repository, clusters, applications and agent_yaml resource and datasource

--- examples/data-sources/harness_platform_gitops_applications/data-source.tf
+++ examples/data-sources/harness_platform_gitops_applications/data-source.tf
@@ -1,8 +1,8 @@
-data "harness_platform_gitops_applications" "example"{
+data "harness_platform_gitops_applications" "example" {
   identifier = "identifier"
   account_id = "account_id"
   project_id = "project_id"
-  org_id = "org_id"
-  agent_id = "agent_id"
-  repo_id = "repo_id"
+  org_id     = "org_id"
+  agent_id   = "agent_id"
+  repo_id    = "repo_id"
 }
