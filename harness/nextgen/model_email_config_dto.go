3ff91b23afe449943fc3c0b8ced542f415a24912
Richa Jajoo <richa.jajoo@harness.io>
2022-11-21 15:46:34 +0530

    SPG-495: Add SendEmailToAllUsers

--- harness/nextgen/model_email_config_dto.go
+++ harness/nextgen/model_email_config_dto.go
@@ -1,7 +1,7 @@
 /*
  * Harness NextGen Software Delivery Platform API Reference
  *
- * This is the Open Api Spec 3 for the NextGen Manager. This is under active development. Beware of the breaking change with respect to the generated code stub  # Authentication  <!-- ReDoc-Inject: <security-definitions> -->
+ * This is the Open Api Spec 3 for the NextGen Manager. This is under active development. Beware of the breaking change with respect to the generated code stub
  *
  * API version: 3.0
  * Contact: contact@harness.io
@@ -10,6 +10,7 @@
 package nextgen
 
 type EmailConfigDto struct {
-	Type_      string `json:"type,omitempty"`
-	GroupEmail string `json:"groupEmail"`
+    Type_ string `json:"type,omitempty"`
+    GroupEmail string `json:"groupEmail"`
+    SendEmailToAllUsers bool `json:"sendEmailToAllUsers"`
 }
