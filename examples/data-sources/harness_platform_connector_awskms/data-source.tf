99a74fc0e2e180e1c505b9ac2b649acd4b23a720
Meet Rathod <rathod.meetsatish@harness.io>
2022-11-27 20:28:25 +0530

    Update data source examples

---
+++ examples/data-sources/harness_platform_connector_awskms/data-source.tf
@@ -0,0 +1,3 @@
+data "harness_platform_connector_awskms" "example" {
+  identifier = "identifier"
+}
---
+++ examples/data-sources/harness_platform_connector_sumologic/data-source.tf
@@ -0,0 +1,3 @@
+data "harness_platform_connector_sumologic" "example" {
+  identifier = "identifier"
+}
