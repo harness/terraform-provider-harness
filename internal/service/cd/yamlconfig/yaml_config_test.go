cce1660e70cb2ea56af0c629849080b81bc57dd1
Meet Rathod <rathod.meetsatish@harness.io>
2022-12-12 20:30:14 +0530

    Fix yaml config resource

--- internal/service/cd/yamlconfig/yaml_config_test.go
+++ internal/service/cd/yamlconfig/yaml_config_test.go
@@ -45,6 +45,41 @@ func TestAccResourceYamlConfig_CloudProvider(t *testing.T) {
 	})
 }
 
+func TestAccResourceYamlConfig_Application(t *testing.T) {
+
+	expectedName := fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(4))
+	updatedName := fmt.Sprintf("%s_updated", t.Name())
+	resourceName := "harness_yaml_config.test"
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccYamlConfigApp(expectedName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", expectedName),
+				),
+			},
+			{
+				Config: testAccYamlConfigApp(updatedName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", updatedName),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateIdFunc: func(s *terraform.State) (string, error) {
+					primary := s.RootModule().Resources[resourceName].Primary
+					return primary.Attributes["path"] + ":" + primary.Attributes["app_id"], nil
+				},
+			},
+		},
+	})
+}
+
 func testAccYamlConfigCloudProvider(name string) string {
 	return fmt.Sprintf(`
 resource "harness_yaml_config" "test" {
@@ -56,6 +91,26 @@ delegateSelectors:
 - k8s
 skipValidation: true
 useKubernetesDelegate: true
+EOF
+}
+`, name)
+}
+
+func testAccYamlConfigApp(name string) string {
+	return fmt.Sprintf(`
+	resource "harness_application" "test" {
+		name = "%[1]s"
+	}
+
+resource "harness_yaml_config" "test" {
+	app_id = harness_application.test.id
+	path = "Setup/Applications/%[1]s+a/Services/%[1]s/Index.yaml"
+	content = <<EOF
+harnessApiVersion: '1.0'
+type: SERVICE
+artifactType: DOCKER
+deploymentType: KUBERNETES
+helmVersion: V2
 EOF
 }
 `, name)
