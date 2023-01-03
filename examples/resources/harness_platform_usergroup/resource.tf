3bc0cd2632f93028814c00a4be4141968ff67cb5
Meet Rathod <rathod.meetsatish@harness.io>
2023-01-03 13:28:37 +0530

    Add example to create usergroup by adding user emails

--- examples/resources/harness_platform_usergroup/resource.tf
+++ examples/resources/harness_platform_usergroup/resource.tf
@@ -61,3 +61,36 @@ resource "harness_platform_usergroup" "sso_type_ldap" {
   linked_sso_type         = "LDAP"
   sso_linked              = true
 }
+
+# Create user group by adding user emails
+resource "harness_platform_usergroup" "example" {
+  identifier         = "identifier"
+  name               = "name"
+  org_id             = "org_id"
+  project_id         = "project_id"
+  linked_sso_id      = "linked_sso_id"
+  externally_managed = false
+  user_emails        = ["user@email.com"]
+  notification_configs {
+    type              = "SLACK"
+    slack_webhook_url = "https://google.com"
+  }
+  notification_configs {
+    type                    = "EMAIL"
+    group_email             = "email@email.com"
+    send_email_to_all_users = true
+  }
+  notification_configs {
+    type                        = "MSTEAMS"
+    microsoft_teams_webhook_url = "https://google.com"
+  }
+  notification_configs {
+    type           = "PAGERDUTY"
+    pager_duty_key = "pagerDutyKey"
+  }
+  linked_sso_display_name = "linked_sso_display_name"
+  sso_group_id            = "sso_group_name" // When sso linked type is saml sso_group_id is same as sso_group_name
+  sso_group_name          = "sso_group_name"
+  linked_sso_type         = "SAML"
+  sso_linked              = true
+}
