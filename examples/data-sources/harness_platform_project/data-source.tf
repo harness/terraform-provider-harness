26a25c19041a83743c34a2ca900808fcf5160d07
Rajendra Baviskar <rajendra.baviskar@harness.io>
2023-01-06 00:31:01 -0800

    SPG-1502 search by name

--- examples/data-sources/harness_platform_project/data-source.tf
+++ examples/data-sources/harness_platform_project/data-source.tf
@@ -2,8 +2,3 @@ data "harness_platform_project" "example_by_id" {
   identifier = "identifier"
   org_id     = "org_id"
 }
-
-data "harness_platform_project" "example_by_name" {
-  name   = "name"
-  org_id = "org_id"
-}
