f9fa6844905c8a450506ae0f2d26cbb9c6992073
Meet Rathod <rathod.meetsatish@harness.io>
2023-01-16 18:19:18 +0530

    Add examples for remote inputset

--- internal/service/platform/input_set/input_set_resource_test.go
+++ internal/service/platform/input_set/input_set_resource_test.go
@@ -107,6 +107,48 @@ func TestAccResourceInputSetRemote(t *testing.T) {
 
 }
 
+func TestAccResourceInputSetInline(t *testing.T) {
+	name := t.Name()
+	id := fmt.Sprintf("%s_%s", name, utils.RandStringBytes(5))
+	updatedName := fmt.Sprintf("%s_updated", name)
+	resourceName := "harness_platform_input_set.test"
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		CheckDestroy:      testAccInputSetDestroy(resourceName),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceInputSetInline(id, name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", id),
+					resource.TestCheckResourceAttr(resourceName, "identifier", id),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "pipeline_id", id),
+				),
+			},
+			{
+				Config: testAccResourceInputSetInline(id, updatedName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", id),
+					resource.TestCheckResourceAttr(resourceName, "identifier", id),
+					resource.TestCheckResourceAttr(resourceName, "name", updatedName),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "pipeline_id", id),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateIdFunc: acctest.PipelineResourceImportStateIdFunc(resourceName),
+			},
+		},
+	})
+
+}
+
 func testAccGetInputSet(resourceName string, state *terraform.State) (*nextgen.InputSetResponseBody, error) {
 	r := acctest.TestAccGetResource(resourceName, state)
 	c, ctx := acctest.TestAccGetClientWithContext()
@@ -139,6 +181,146 @@ func buildField(r *terraform.ResourceState, field string) optional.String {
 		return optional.NewString(attr)
 	}
 	return optional.EmptyString()
+}
+
+func testAccResourceInputSetInline(id string, name string) string {
+	return fmt.Sprintf(`
+    resource "harness_platform_organization" "test" {
+        identifier = "%[1]s"
+        name = "%[2]s"
+    }
+
+    resource "harness_platform_project" "test" {
+        identifier = "%[1]s"
+        name = "%[2]s"
+        org_id = harness_platform_organization.test.id
+        color = "#472848"
+    }
+    resource "harness_platform_pipeline" "test" {
+        identifier = "%[1]s"
+        org_id = harness_platform_project.test.org_id
+        project_id = harness_platform_project.test.id
+        name = "%[2]s"
+yaml = <<-EOT
+pipeline:
+    name: %[2]s
+    identifier: %[1]s
+    allowStageExecutions: false
+    projectIdentifier: ${harness_platform_project.test.id}
+    orgIdentifier: ${harness_platform_project.test.org_id}
+    tags: {}
+    stages:
+        - stage:
+            name: dep
+            identifier: dep
+            description: ""
+            type: Deployment
+            spec:
+                serviceConfig:
+                    serviceRef: service
+                    serviceDefinition:
+                        type: Kubernetes
+                        spec:
+                            variables: []
+                infrastructure:
+                    environmentRef: testenv
+                    infrastructureDefinition:
+                        type: KubernetesDirect
+                        spec:
+                            connectorRef: testconf
+                            namespace: test
+                            releaseName: release-<+INFRA_KEY>
+                    allowSimultaneousDeployments: false
+                execution:
+                    steps:
+                        - stepGroup:
+                                name: Canary Deployment
+                                identifier: canaryDepoyment
+                                steps:
+                                    - step:
+                                        name: Canary Deployment
+                                        identifier: canaryDeployment
+                                        type: K8sCanaryDeploy
+                                        timeout: 10m
+                                        spec:
+                                            instanceSelection:
+                                                type: Count
+                                                spec:
+                                                    count: 1
+                                            skipDryRun: false
+                                    - step:
+                                        name: Canary Delete
+                                        identifier: canaryDelete
+                                        type: K8sCanaryDelete
+                                        timeout: 10m
+                                        spec: {}
+                                rollbackSteps:
+                                    - step:
+                                        name: Canary Delete
+                                        identifier: rollbackCanaryDelete
+                                        type: K8sCanaryDelete
+                                        timeout: 10m
+                                        spec: {}
+                        - stepGroup:
+                                name: Primary Deployment
+                                identifier: primaryDepoyment
+                                steps:
+                                    - step:
+                                        name: Rolling Deployment
+                                        identifier: rollingDeployment
+                                        type: K8sRollingDeploy
+                                        timeout: 10m
+                                        spec:
+                                            skipDryRun: false
+                                rollbackSteps:
+                                    - step:
+                                        name: Rolling Rollback
+                                        identifier: rollingRollback
+                                        type: K8sRollingRollback
+                                        timeout: 10m
+                                        spec: {}
+                    rollbackSteps: []
+            tags: {}
+            failureStrategies:
+                - onFailure:
+                        errors:
+                            - AllErrors
+                        action:
+                            type: StageRollback
+    variables:
+        - name: key
+          type: String
+          default: value
+          value: <+input>.allowedValues(value)
+                EOT
+}
+
+resource "harness_platform_input_set" "test" {
+    identifier = "%[1]s"
+    name = "%[2]s"
+    tags = [
+        "foo:bar",
+    ]
+    org_id = harness_platform_organization.test.id
+    project_id = harness_platform_project.test.id
+    pipeline_id = harness_platform_pipeline.test.id
+    yaml = <<-EOT
+inputSet:
+  identifier: "%[1]s"
+  name: "%[2]s"
+  tags:
+    foo: "bar"
+  orgIdentifier: "${harness_platform_organization.test.id}"
+  projectIdentifier: "${harness_platform_project.test.id}"
+  pipeline:
+    identifier: "${harness_platform_pipeline.test.id}"
+    variables:
+    - name: "key"
+      type: "String"
+      value: "value"
+EOT
+}
+    `, id, name)
 }
 
 func testAccResourceInputSetRemote(id string, name string) string {
