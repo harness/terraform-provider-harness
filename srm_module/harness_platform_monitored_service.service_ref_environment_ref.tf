355073b9fb08ad101571cb5b4d91d2224ba946d0
Deepak Chhikara <deepak.chhikara@harness.io>
2023-01-20 11:32:28 +0530

    SRM Demo

--- srm_module/harness_platform_monitored_service.service_ref_environment_ref.tf
+++ srm_module/harness_platform_monitored_service.service_ref_environment_ref.tf
@@ -1,4 +1,8 @@
 resource "harness_platform_monitored_service" "service_ref_environment_ref" {
+  depends_on = [
+    harness_platform_connector_prometheus.connectorRef
+  ]
+
   org_id     = "terraform_org"
   project_id = "terraform_project"
   identifier = "service_ref_environment_ref"
