5e63344ba29b29f9002fac647defe6f1e968c94b
Richa Jajoo <richa.jajoo@harness.io>
2023-02-24 15:42:37 +0530

    PL-31629: GCP Secret Manager terraform resource showing updated default value

--- internal/service/platform/connector/gcp_secret_manager_test.go
+++ internal/service/platform/connector/gcp_secret_manager_test.go
@@ -57,6 +57,54 @@ func TestAccResourceConnectorGcpSM(t *testing.T) {
 	})
 }
 
+func TestAccResourceConnectorGcpSMDefault(t *testing.T) {
+
+	id := fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(5))
+	name := id
+	updatedName := fmt.Sprintf("%s_updated", name)
+	resourceName := "harness_platform_connector_gcp_secret_manager.test"
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"time": {},
+		},
+		CheckDestroy: testAccConnectorDestroy(resourceName),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceConnectorGcpSMDefault(id, name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", id),
+					resource.TestCheckResourceAttr(resourceName, "identifier", id),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "description", "test"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "delegate_selectors.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "is_default", "true"),
+				),
+			},
+			{
+				Config: testAccResourceConnectorGcpSMDefault(id, updatedName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", id),
+					resource.TestCheckResourceAttr(resourceName, "identifier", id),
+					resource.TestCheckResourceAttr(resourceName, "name", updatedName),
+					resource.TestCheckResourceAttr(resourceName, "description", "test"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "delegate_selectors.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "is_default", "true"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccResourceConnectorGcpSM(id string, name string) string {
 	return fmt.Sprintf(`
 		resource "harness_platform_secret_text" "test" {
@@ -74,6 +122,37 @@ func testAccResourceConnectorGcpSM(id string, name string) string {
 			name = "%[2]s"
 			description = "test"
 			tags = ["foo:bar"]
+		
+			delegate_selectors = ["harness-delegate"]
+			credentials_ref = "account.${harness_platform_secret_text.test.id}"
+			depends_on = [time_sleep.wait_4_seconds]
+		}
+
+		resource "time_sleep" "wait_4_seconds" {
+			depends_on = [harness_platform_secret_text.test]
+			destroy_duration = "4s"
+		}
+`, id, name)
+}
+
+func testAccResourceConnectorGcpSMDefault(id string, name string) string {
+	return fmt.Sprintf(`
+		resource "harness_platform_secret_text" "test" {
+			identifier = "%[1]s"
+			name = "%[1]s"
+			description = "test"
+			tags = ["foo:bar"]
+			secret_manager_identifier = "azureSecretManager"
+			value_type = "Reference"
+			value = "secret"
+		}
+
+		resource "harness_platform_connector_gcp_secret_manager" "test" {
+			identifier = "%[1]s"
+			name = "%[2]s"
+			description = "test"
+			tags = ["foo:bar"]
+			is_default = true
 		
 			delegate_selectors = ["harness-delegate"]
 			credentials_ref = "account.${harness_platform_secret_text.test.id}"
