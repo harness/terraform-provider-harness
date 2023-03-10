f2a5d9a7d0196af8d89c6a51b4a8a2240e5a2e2a
adiyaar24 <aditya.kashyap@harness.io>
2023-03-10 12:50:20 +0530

    Removed Redudant Code

--- internal/service/platform/usergroup/usergroup.go
+++ internal/service/platform/usergroup/usergroup.go
@@ -169,11 +169,11 @@ func resourceUserGroupRead(ctx context.Context, d *schema.ResourceData, meta int
 
 	// Soft delete lookup error handling
 	// https://harness.atlassian.net/browse/PL-23765
-	if resp.Data == nil {
-		d.SetId("")
-		d.MarkNewResource()
-		return nil
-	}
+	// if resp.Data == nil {
+	// 	d.SetId("")
+	// 	d.MarkNewResource()
+	// 	return nil
+	// }
 
 	if attr, ok := d.GetOk("user_emails"); ok {
 		d.Set("user_emails", attr)
