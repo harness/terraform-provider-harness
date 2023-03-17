a994467981b2597b9a400801eda1eb3b26c5d19a
Aditya Kashyap <122971747+adiyaar24@users.noreply.github.com>
2023-03-17 12:44:07 +0530

    Update resource_roles_test.go

--- internal/service/platform/roles/resource_roles_test.go
+++ internal/service/platform/roles/resource_roles_test.go
@@ -33,58 +33,11 @@ func TestAccResourceRoles(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "description", "test"),
 					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "permissions.0", "core_pipeline_edit"),
-					resource.TestCheckResourceAttr(resourceName, "allowed_scope_levels.0", "account"),
-				),
-			},
-			{
-				Config: testAccResourceRoles(id, updatedName),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "id", id),
-					resource.TestCheckResourceAttr(resourceName, "name", updatedName),
-					resource.TestCheckResourceAttr(resourceName, "description", "test"),
-					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "permissions.0", "core_pipeline_edit"),
-					resource.TestCheckResourceAttr(resourceName, "allowed_scope_levels.0", "account"),
-				),
-			},
-			{
-				ResourceName:      resourceName,
-				ImportState:       true,
-				ImportStateVerify: true,
-				ImportStateIdFunc: acctest.ProjectResourceImportStateIdFunc(resourceName),
-				ImportStateVerifyIgnore: []string{"org_id","project_id"},
-			},
-		},
-	})
-
-}
-
-func TestOrgResourceRoles(t *testing.T) {
-
-	name := t.Name()
-	id := fmt.Sprintf("%s_%s", name, utils.RandStringBytes(5))
-	updatedName := fmt.Sprintf("%s_updated", name)
-
-	resourceName := "harness_platform_roles.test"
-
-	resource.UnitTest(t, resource.TestCase{
-		PreCheck:          func() { acctest.TestAccPreCheck(t) },
-		ProviderFactories: acctest.ProviderFactories,
-		CheckDestroy:      testAccRolesDestroy(resourceName),
-		Steps: []resource.TestStep{
-			{
-				Config: testOrgResourceRoles(id, name),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "id", id),
-					resource.TestCheckResourceAttr(resourceName, "name", name),
-					resource.TestCheckResourceAttr(resourceName, "description", "test"),
-					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "permissions.0", "core_pipeline_edit"),
 					resource.TestCheckResourceAttr(resourceName, "allowed_scope_levels.0", "project"),
 				),
 			},
 			{
-				Config: testOrgResourceRoles(id, updatedName),
+				Config: testAccResourceRoles(id, updatedName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "id", id),
 					resource.TestCheckResourceAttr(resourceName, "name", updatedName),
@@ -141,20 +94,6 @@ func buildField(r *terraform.ResourceState, field string) optional.String {
 }
 
 func testAccResourceRoles(id string, name string) string {
-	return fmt.Sprintf(`
-
-	resource "harness_platform_roles" "test" {
-		identifier = "%[1]s"
-		name = "%[2]s"
-		description = "test"
-		tags = ["foo:bar"]
-		permissions = ["core_pipeline_edit"]
-		allowed_scope_levels = ["account"]
-	}
-`, id, name)
-}
-
-func testOrgResourceRoles(id string, name string) string {
 	return fmt.Sprintf(`
 	resource "harness_platform_organization" "test" {
 		identifier = "%[1]s"
