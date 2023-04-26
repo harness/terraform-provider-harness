3af7fb87f5636952f371666f9d565160d11fa849
tuodor.macari@harness.io <tudor.macari@harness.io>
2023-04-26 13:41:35 +0300

    [CDS-54508]: add test

--- internal/service/platform/connector/terraform_cloud_test.go
+++ internal/service/platform/connector/terraform_cloud_test.go
@@ -4,15 +4,48 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/harness/harness-go-sdk/harness/nextgen"
 	"github.com/harness/harness-go-sdk/harness/utils"
 	"github.com/harness/terraform-provider-harness/internal/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAccResourceConnectorTerraformCloud(t *testing.T) {
 	id := fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(5))
 	name := id
 	executeTfCloudTest(t, testAccResourceConnectorTerraformCloud(id, name), "true", id, name)
+}
+
+func TestAccResourceConnectorTerraformCloud_DeleteUnderlyingResource(t *testing.T) {
+	id := fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(5))
+	name := id
+	resourceName := "harness_platform_connector_terraform_cloud.test"
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceConnectorTerraformCloudNoDependency(id, name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", id),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+				),
+			},
+			{
+				PreConfig: func() {
+					acctest.TestAccConfigureProvider()
+					c, ctx := acctest.TestAccGetPlatformClientWithContext()
+					_, _, err := c.ConnectorsApi.DeleteConnector(ctx, c.AccountId, id, &nextgen.ConnectorsApiDeleteConnectorOpts{})
+					require.NoError(t, err)
+				},
+				Config:             testAccResourceConnectorTerraformCloudNoDependency(id, name),
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
 }
 
 func executeTfCloudTest(t *testing.T, config string, executeOnDelegate string, id string, name string) {
@@ -45,6 +78,35 @@ func executeTfCloudTest(t *testing.T, config string, executeOnDelegate string, i
 			},
 		},
 	})
+}
+
+func testAccResourceConnectorTerraformCloudNoDependency(id string, name string) string {
+	return fmt.Sprintf(`
+		resource "harness_platform_secret_text" "test" {
+			identifier = "%[1]s"
+			name = "%[2]s"
+			description = "test"
+			tags = ["foo:bar"]
+
+			secret_manager_identifier = "harnessSecretManager"
+			value_type = "Inline"
+			value = "secret"
+		}
+
+		resource "harness_platform_connector_terraform_cloud" "test" {
+			url = "https://app.terraform.io/"
+			identifier = "%[1]s"
+			name = "%[2]s"
+			description = "test"
+			tags = ["foo:bar"]
+			delegate_selectors = ["harness-delegate"]
+			credentials {
+				api_token {
+					api_token_ref = "account.${harness_platform_secret_text.test.id}"
+				}
+			}
+		}
+`, id, name)
 }
 
 func testAccResourceConnectorTerraformCloud(id string, name string) string {
