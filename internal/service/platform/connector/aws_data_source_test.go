0a05e89adb6e5357d1c3cbbbf3585053aa9084dd
Rishabh Gupta <rishabh.gupta@harness.io>
2023-06-06 14:58:09 +0530

    feat: [CDS-59495]: add support for aws backoff strategy override (#560)
    * feat: [CDS-59495]: add support for aws backoff strategy override
    
    * feat: [CDS-59495]: add support for aws backoff strategy override
    
    * feat: [CDS-59495]: increment go sdk version
    
    * feat: [CDS-59495]: add description

--- internal/service/platform/connector/aws_data_source_test.go
+++ internal/service/platform/connector/aws_data_source_test.go
@@ -35,6 +35,92 @@ func TestAccDataSourceConnectorAws(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceConnectorAwsFullJitterBackOff(t *testing.T) {
+
+	var (
+		name         = fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(4))
+		resourceName = "data.harness_platform_connector_aws.test"
+	)
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceConnectorAwsFullJitterBackOff(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", name),
+					resource.TestCheckResourceAttr(resourceName, "identifier", name),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "description", "test"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "inherit_from_delegate.0.delegate_selectors.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "full_jitter_backoff_strategy.0.retry_count", "3"),
+					resource.TestCheckResourceAttr(resourceName, "full_jitter_backoff_strategy.0.base_delay", "10"),
+					resource.TestCheckResourceAttr(resourceName, "full_jitter_backoff_strategy.0.max_backoff_time", "65"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceConnectorAwsEqualJitterBackOff(t *testing.T) {
+
+	var (
+		name         = fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(4))
+		resourceName = "data.harness_platform_connector_aws.test"
+	)
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceConnectorAwsEqualJitterBackOff(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", name),
+					resource.TestCheckResourceAttr(resourceName, "identifier", name),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "description", "test"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "inherit_from_delegate.0.delegate_selectors.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "equal_jitter_backoff_strategy.0.retry_count", "3"),
+					resource.TestCheckResourceAttr(resourceName, "equal_jitter_backoff_strategy.0.base_delay", "10"),
+					resource.TestCheckResourceAttr(resourceName, "equal_jitter_backoff_strategy.0.max_backoff_time", "65"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceConnectorAwsFixedDelayBackOff(t *testing.T) {
+
+	var (
+		name         = fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(4))
+		resourceName = "data.harness_platform_connector_aws.test"
+	)
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceConnectorAwsFixedDelayBackOff(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", name),
+					resource.TestCheckResourceAttr(resourceName, "identifier", name),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "description", "test"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "inherit_from_delegate.0.delegate_selectors.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "fixed_delay_backoff_strategy.0.retry_count", "3"),
+					resource.TestCheckResourceAttr(resourceName, "fixed_delay_backoff_strategy.0.fixed_backoff", "10"),
+				),
+			},
+		},
+	})
+}
+
 func testAccDataSourceConnectorAws(name string) string {
 	return fmt.Sprintf(`
 		resource "harness_platform_connector_aws" "test" {
@@ -45,6 +131,77 @@ func testAccDataSourceConnectorAws(name string) string {
 
 			inherit_from_delegate {
 				delegate_selectors = ["harness-delegate"]
+			}
+		}
+
+		data "harness_platform_connector_aws" "test" {
+			identifier = harness_platform_connector_aws.test.identifier
+		}
+	`, name)
+}
+
+func testAccDataSourceConnectorAwsEqualJitterBackOff(name string) string {
+	return fmt.Sprintf(`
+		resource "harness_platform_connector_aws" "test" {
+			identifier = "%[1]s"
+			name = "%[1]s"
+			description = "test"
+			tags = ["foo:bar"]
+
+			inherit_from_delegate {
+				delegate_selectors = ["harness-delegate"]
+			}
+			equal_jitter_backoff_strategy {
+				base_delay = 10
+				max_backoff_time = 65
+				retry_count = 3
+			}
+		}
+
+		data "harness_platform_connector_aws" "test" {
+			identifier = harness_platform_connector_aws.test.identifier
+		}
+	`, name)
+}
+
+func testAccDataSourceConnectorAwsFullJitterBackOff(name string) string {
+	return fmt.Sprintf(`
+		resource "harness_platform_connector_aws" "test" {
+			identifier = "%[1]s"
+			name = "%[1]s"
+			description = "test"
+			tags = ["foo:bar"]
+
+			inherit_from_delegate {
+				delegate_selectors = ["harness-delegate"]
+			}
+			full_jitter_backoff_strategy {
+				base_delay = 10
+				max_backoff_time = 65
+				retry_count = 3
+			}
+		}
+
+		data "harness_platform_connector_aws" "test" {
+			identifier = harness_platform_connector_aws.test.identifier
+		}
+	`, name)
+}
+
+func testAccDataSourceConnectorAwsFixedDelayBackOff(name string) string {
+	return fmt.Sprintf(`
+		resource "harness_platform_connector_aws" "test" {
+			identifier = "%[1]s"
+			name = "%[1]s"
+			description = "test"
+			tags = ["foo:bar"]
+
+			inherit_from_delegate {
+				delegate_selectors = ["harness-delegate"]
+			}
+			fixed_delay_backoff_strategy {
+				fixed_backoff = 10
+				retry_count = 3
 			}
 		}
 
