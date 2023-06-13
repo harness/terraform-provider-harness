cdeb70d5a4993c38148030dd2a1777e210f5daf4
vivekkarnatiharness <vivek.karnati@harness.io>
2023-06-13 14:54:45 +0530

    fix: [CDS-70013]: fix infrastruture import to work at infra and org level

--- helpers/schema.go
+++ helpers/schema.go
@@ -207,13 +207,33 @@ var TriggerResourceImporter = &schema.ResourceImporter{
 var EnvRelatedResourceImporter = &schema.ResourceImporter{
 	State: func(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
 		parts := strings.Split(d.Id(), "/")
-		d.Set("org_id", parts[0])
-		d.Set("project_id", parts[1])
-		d.Set("env_id", parts[2])
-		d.Set("identifier", parts[3])
-		d.SetId(parts[3])
+		partCount := len(parts)
+		isAccountConnector := partCount == 2
+		isOrgConnector := partCount == 3
+		isProjectConnector := partCount == 4
+		if isAccountConnector {
+			d.Set("env_id", parts[0])
+			d.Set("identifier", parts[1])
+			d.SetId(parts[1])
+			return []*schema.ResourceData{d}, nil
+		}
+		if isOrgConnector {
+			d.Set("org_id", parts[0])
+			d.Set("env_id", parts[1])
+			d.Set("identifier", parts[2])
+			d.SetId(parts[2])
+			return []*schema.ResourceData{d}, nil
+		}
+		if isProjectConnector {
+			d.Set("org_id", parts[0])
+			d.Set("project_id", parts[1])
+			d.Set("env_id", parts[2])
+			d.Set("identifier", parts[3])
+			d.SetId(parts[3])
+			return []*schema.ResourceData{d}, nil
+		}
 
-		return []*schema.ResourceData{d}, nil
+		return nil, fmt.Errorf("invalid identifier: %s", d.Id())
 	},
 }
 
