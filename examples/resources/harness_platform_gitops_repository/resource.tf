27a46788b0d001fa78a4a3387b355656f899dca5
Mirko Teodorovic <mirko.teodorovic@harness.io>
2023-06-19 13:04:06 +0200

    feat: [CDS-70737]: add repository examples
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@harness.io>

--- examples/resources/harness_platform_gitops_repository/resource.tf
+++ examples/resources/harness_platform_gitops_repository/resource.tf
@@ -1,3 +1,4 @@
+// Create a git repository at project level
 resource "harness_platform_gitops_repository" "example" {
   identifier = "identifier"
   account_id = "account_id"
@@ -9,6 +10,43 @@ resource "harness_platform_gitops_repository" "example" {
     name            = "repo_name"
     insecure        = true
     connection_type = "HTTPS_ANONYMOUS"
+    type            = "git"
+  }
+  upsert = true
+}
+
+// Create a HELM repository at project level
+resource "harness_platform_gitops_repository" "example" {
+  identifier = "identifier"
+  account_id = "account_id"
+  project_id = "project_id"
+  org_id     = "org_id"
+  agent_id   = "agent_id"
+  repo {
+    repo            = "https://charts.helm.sh/stable"
+    name            = "repo_name"
+    insecure        = true
+    connection_type = "HTTPS_ANONYMOUS"
+    type            = "helm"
+  }
+  upsert = true
+}
+
+// Create a OCI HELM repository at project level
+resource "harness_platform_gitops_repository" "example" {
+  identifier = "identifier"
+  account_id = "account_id"
+  project_id = "project_id"
+  org_id     = "org_id"
+  agent_id   = "agent_id"
+  repo {
+    repo            = "ghcr.io/wings-software"
+    name            = "repo_name"
+    insecure        = true
+    username        = "username",
+    password        = "ghp_xxxxxxxx",
+    type            = "helm"
+    enableOCI       = true
   }
   upsert = true
 }
