78a46946b5734f964f5bf97c057aa056411fd03a
Naman Goenka <naman.goenka@harness.io>
2023-06-23 15:40:00 +0530

    feat:[CDS-57089]: added Jira Pat auth type

--- examples/resources/harness_platform_connector_jira/resource.tf
+++ examples/resources/harness_platform_connector_jira/resource.tf
@@ -1,3 +1,4 @@
+# Auth type UsernamePassword
 resource "harness_platform_connector_jira" "test" {
   identifier  = "identifier"
   name        = "name"
@@ -11,6 +12,23 @@ resource "harness_platform_connector_jira" "test" {
     username_password {
       username     = "admin"
       password_ref = "account.secret_id"
+    }
+  }
+}
+
+# Auth Type PersonalAccessToken
+resource "harness_platform_connector_jira" "test" {
+  identifier  = "identifier"
+  name        = "name"
+  description = "test"
+  tags        = ["foo:bar"]
+
+  url                = "https://jira.com"
+  delegate_selectors = ["harness-delegate"]
+  auth {
+    auth_type = "PersonalAccessToken"
+    personal_access_token {
+      pat_ref = "account.secret_id"
     }
   }
 }
