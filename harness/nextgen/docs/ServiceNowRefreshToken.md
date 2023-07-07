0a249eb9d7482326878d4f056ceb35dc5bd2338d
Naman Goenka <naman.goenka@harness.io>
2023-07-07 11:03:26 +0530

    feat:[CDS-69871]: supported servicenow refresh token auth type

---
+++ harness/nextgen/docs/ServiceNowRefreshToken.md
@@ -0,0 +1,13 @@
+# ServiceNowRefreshToken
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**TokenUrl** | **string** |  | [default to null]
+**RefreshTokenRef** | **string** |  | [default to null]
+**ClientIdRef** | **string** |  | [default to null]
+**ClientSecretRef** | **string** |  | [optional] [default to null]
+**Scope** | **string** |  | [optional] [default to null]
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
--- harness/nextgen/enum_service_now_auth_types.go
+++ harness/nextgen/enum_service_now_auth_types.go
@@ -5,14 +5,17 @@ type ServiceNowAuthType string
 var ServiceNowAuthTypes = struct {
 	ServiceNowUserNamePassword ServiceNowAuthType
 	ServiceNowAdfs             ServiceNowAuthType
+	ServiceNowRefreshToken     ServiceNowAuthType
 }{
 	ServiceNowUserNamePassword: "UsernamePassword",
 	ServiceNowAdfs:             "AdfsClientCredentialsWithCertificate",
+	ServiceNowRefreshToken:     "RefreshTokenGrantType",  
 }
 
 var ServiceNowAuthTypeValues = []string{
 	ServiceNowAuthTypes.ServiceNowUserNamePassword.String(),
 	ServiceNowAuthTypes.ServiceNowAdfs.String(),
+	ServiceNowAuthTypes.ServiceNowRefreshToken.String(),
 }
 
 func (e ServiceNowAuthType) String() string {
