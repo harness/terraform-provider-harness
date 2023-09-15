08bce7d97ed77bae80d26a19641ab5f393dc5523
shalinlk <shalin.lk@harness.io>
2023-09-15 14:08:32 +0530

    Adding example files

--- examples/resources/harness_autostopping_aws_alb/resource.tf
+++ examples/resources/harness_autostopping_aws_alb/resource.tf
@@ -1,10 +1,10 @@
 resource "harness_autostopping_aws_alb" "test" {
-			name = "name"
-			cloud_connector_id = "cloud_connector_id"
-			host_name = "host_name"
-      		region = "region"
-			vpc = "vpc"
-			security_groups =["sg1","sg2"]
-			route53_hosted_zone_id = "/hostedzone/zone_id"
+  name                   = "name"
+  cloud_connector_id     = "cloud_connector_id"
+  host_name              = "host_name"
+  region                 = "region"
+  vpc                    = "vpc"
+  security_groups        = ["sg1", "sg2"]
+  route53_hosted_zone_id = "/hostedzone/zone_id"
 }
 
--- examples/resources/harness_autostopping_azure_gateway/resource.tf
+++ examples/resources/harness_autostopping_azure_gateway/resource.tf
@@ -1,12 +1,12 @@
 resource "harness_autostopping_azure_gateway" "test" {
-		name = "name"
-		cloud_connector_id = "cloud_connector_id"
-		host_name = "host_name"
-		region = "eastus2"
-		resource_group = "resource_group"
-		subnet_id = "/subscriptions/subscription_id/resourceGroups/resource_group/providers/Microsoft.Network/virtualNetworks/virtual_network/subnets/subnet_id"
-		vpc = "/subscriptions/subscription_id/resourceGroups/resource_group/providers/Microsoft.Network/virtualNetworks/virtual_network"
-		azure_func_region = "westus2"
-		frontend_ip = "/subscriptions/subscription_id/resourceGroups/resource_group/providers/Microsoft.Network/publicIPAddresses/publicip"
-		sku_size = "sku2"
-	}
+  name               = "name"
+  cloud_connector_id = "cloud_connector_id"
+  host_name          = "host_name"
+  region             = "eastus2"
+  resource_group     = "resource_group"
+  subnet_id          = "/subscriptions/subscription_id/resourceGroups/resource_group/providers/Microsoft.Network/virtualNetworks/virtual_network/subnets/subnet_id"
+  vpc                = "/subscriptions/subscription_id/resourceGroups/resource_group/providers/Microsoft.Network/virtualNetworks/virtual_network"
+  azure_func_region  = "westus2"
+  frontend_ip        = "/subscriptions/subscription_id/resourceGroups/resource_group/providers/Microsoft.Network/publicIPAddresses/publicip"
+  sku_size           = "sku2"
+}
