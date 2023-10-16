b444ae1cd1ba08cc097711376b76de59cdcc4162
shalinlk <shalin.lk@harness.io>
2023-10-16 22:21:28 +0530

    Refactoring

--- internal/service/platform/autostopping/schedule/schedule.go
+++ internal/service/platform/autostopping/schedule/schedule.go
@@ -84,6 +84,40 @@ func timeValidateFunc(i interface{}, p cty.Path) diag.Diagnostics {
 	return diags
 }
 
+func daysValidationFunc(i interface{}, p cty.Path) diag.Diagnostics {
+	diags := diag.Diagnostics{}
+	v, ok := i.(string)
+	if !ok {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Value is mandatory and should be string",
+		})
+		return diags
+	}
+	parts := strings.Split(v, ",")
+	valids := []string{"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"}
+	for _, p := range parts {
+		vp := strings.TrimSpace(p)
+		match := false
+		for _, vv := range valids {
+			match = match || strings.EqualFold(vp, vv)
+		}
+		if !match {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  "Valid input is comma separated values of `SUN`, `MON`, `TUE`, `WED`, `THU`, `FRI` and `SAT`. Eg : `MON,TUE,WED,THU,FRI` for Mon through Friday ",
+			})
+		}
+	}
+	if len(valids) < 1 || len(valids) > 7 {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "At-least one and at-most seven days can be specified",
+		})
+	}
+	return diags
+}
+
 func ResourceVMRule() *schema.Resource {
 	resource := &schema.Resource{
 		Description: "Resource for creating a Harness Variables.",
@@ -143,42 +177,10 @@ func ResourceVMRule() *schema.Resource {
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"days": {
-							Description: "Days on which schedule need to be active. Comma separated values of `SUN`, `MON`, `TUE`, `WED`, `THU`, `FRI` and `SAT`. Eg : `MON,TUE,WED,THU,FRI` for Mon through Friday",
-							Type:        schema.TypeString,
-							Required:    true,
-							ValidateDiagFunc: func(i interface{}, p cty.Path) diag.Diagnostics {
-								diags := diag.Diagnostics{}
-								v, ok := i.(string)
-								if !ok {
-									diags = append(diags, diag.Diagnostic{
-										Severity: diag.Error,
-										Summary:  "Value is mandatory and should be string",
-									})
-									return diags
-								}
-								parts := strings.Split(v, ",")
-								valids := []string{"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"}
-								for _, p := range parts {
-									vp := strings.TrimSpace(p)
-									match := false
-									for _, vv := range valids {
-										match = match || strings.EqualFold(vp, vv)
-									}
-									if !match {
-										diags = append(diags, diag.Diagnostic{
-											Severity: diag.Error,
-											Summary:  "Valid input is comma separated values of `SUN`, `MON`, `TUE`, `WED`, `THU`, `FRI` and `SAT`. Eg : `MON,TUE,WED,THU,FRI` for Mon through Friday ",
-										})
-									}
-								}
-								if len(valids) < 1 || len(valids) > 7 {
-									diags = append(diags, diag.Diagnostic{
-										Severity: diag.Error,
-										Summary:  "At-least one and at-most seven days can be specified",
-									})
-								}
-								return diags
-							},
+							Description:      "Days on which schedule need to be active. Comma separated values of `SUN`, `MON`, `TUE`, `WED`, `THU`, `FRI` and `SAT`. Eg : `MON,TUE,WED,THU,FRI` for Mon through Friday",
+							Type:             schema.TypeString,
+							Required:         true,
+							ValidateDiagFunc: daysValidationFunc,
 						},
 						"start_time": {
 							Description:      "Starting time of schedule action on the day. Accepted format is HH:MM. Eg : 13:15 for 01:15pm",
