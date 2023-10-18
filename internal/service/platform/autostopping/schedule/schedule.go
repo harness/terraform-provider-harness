0b82efaca94624f3b7bd07479f66f9aa8d76d35a
shalinlk <shalin.lk@harness.io>
2023-10-18 17:44:38 +0530

    Refactoring

--- internal/service/platform/autostopping/schedule/schedule.go
+++ internal/service/platform/autostopping/schedule/schedule.go
@@ -56,6 +56,100 @@ var (
 	}
 )
 
+func ResourceVMRule() *schema.Resource {
+	resource := &schema.Resource{
+		Description: "Resource for creating a Harness Variables.",
+
+		ReadContext:   resourceScheduleRead,
+		UpdateContext: resourceScheduleUpdate,
+		DeleteContext: resourceScheduleDelete,
+		CreateContext: resourceScheduleCreate,
+		Importer:      helpers.MultiLevelResourceImporter,
+		Schema: map[string]*schema.Schema{
+			"identifier": {
+				Description: "Unique identifier of the resource",
+				Type:        schema.TypeFloat,
+				Computed:    true,
+			},
+			nameAttribute: {
+				Description: "Name of the schedule",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			scheduleTypeAttribute: {
+				Description:  fmt.Sprintf("Type of the schedule. Valid values are `%s` and `%s`", uptimeSchedule, downtimeSchedule),
+				Type:         schema.TypeString,
+				Required:     true,
+				ExactlyOneOf: []string{string(uptimeSchedule), string(downtimeSchedule)},
+			},
+			timeZoneAttribute: {
+				Description: "Time zone in which schedule needs to be executed",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			timePeriodAttribute: {
+				Description: "Time period in which schedule will be active. If specified along with periodicity, this will act as the boundary of periodicity. Otherwise schedule action will be triggered at `start` time and terminate at `end` time.",
+				Type:        schema.TypeList,
+				Optional:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						startAttribute: {
+							Description:      "Time from which schedule will be active. Need to be in YYYY-MM-DD HH:mm:SS format. Eg 2006-01-02 15:04:05",
+							Type:             schema.TypeString,
+							Required:         true,
+							ValidateDiagFunc: dateValidation,
+						},
+						endAttribute: {
+							Description:      "Time until which schedule will be active. Need to be in YYYY-MM-DD HH:mm:SS format. Eg 2006-01-02 15:04:05",
+							Type:             schema.TypeString,
+							Required:         true,
+							ValidateDiagFunc: dateValidation,
+						},
+					},
+				},
+			},
+			periodicityAttribute: {
+				Description: "For defining periodic schedule. Periodic nature will be applicable from the time of creation of schedule, unless specific 'time_period' is specified",
+				Type:        schema.TypeList,
+				Optional:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						daysAttribute: {
+							Description:      "Days on which schedule need to be active. Comma separated values of `SUN`, `MON`, `TUE`, `WED`, `THU`, `FRI` and `SAT`. Eg : `MON,TUE,WED,THU,FRI` for Mon through Friday",
+							Type:             schema.TypeString,
+							Required:         true,
+							ValidateDiagFunc: daysValidationFunc,
+						},
+						startTimeAttribute: {
+							Description:      "Starting time of schedule action on the day. Accepted format is HH:MM. Eg : 13:15 for 01:15pm",
+							Type:             schema.TypeString,
+							Required:         true,
+							ValidateDiagFunc: timeValidation,
+						},
+						endTimeAttribute: {
+							Description:      "Ending time of schedule action on the day. Accepted format is HH:MM. Eg : 20:00 for 8pm",
+							Type:             schema.TypeString,
+							Required:         true,
+							ValidateDiagFunc: timeValidation,
+						},
+					},
+				},
+			},
+			rulesAttribute: {
+				Description: "ID of AutoStopping rules on which the schedule applies",
+				Required:    true,
+				Type:        schema.TypeList,
+				MinItems:    1,
+				Elem: &schema.Schema{
+					Type: schema.TypeFloat,
+				},
+			},
+		},
+	}
+
+	return resource
+}
+
 func dateValidation(i interface{}, p cty.Path) diag.Diagnostics {
 	diags := diag.Diagnostics{}
 	v, _ := i.(string)
@@ -160,100 +254,6 @@ func daysValidationFunc(i interface{}, p cty.Path) diag.Diagnostics {
 		})
 	}
 	return diags
-}
-
-func ResourceVMRule() *schema.Resource {
-	resource := &schema.Resource{
-		Description: "Resource for creating a Harness Variables.",
-
-		ReadContext:   resourceScheduleRead,
-		UpdateContext: resourceScheduleUpdate,
-		DeleteContext: resourceScheduleDelete,
-		CreateContext: resourceScheduleCreate,
-		Importer:      helpers.MultiLevelResourceImporter,
-		Schema: map[string]*schema.Schema{
-			"identifier": {
-				Description: "Unique identifier of the resource",
-				Type:        schema.TypeFloat,
-				Computed:    true,
-			},
-			nameAttribute: {
-				Description: "Name of the schedule",
-				Type:        schema.TypeString,
-				Required:    true,
-			},
-			scheduleTypeAttribute: {
-				Description:  fmt.Sprintf("Type of the schedule. Valid values are `%s` and `%s`", uptimeSchedule, downtimeSchedule),
-				Type:         schema.TypeString,
-				Required:     true,
-				ExactlyOneOf: []string{string(uptimeSchedule), string(downtimeSchedule)},
-			},
-			timeZoneAttribute: {
-				Description: "Time zone in which schedule needs to be executed",
-				Type:        schema.TypeString,
-				Required:    true,
-			},
-			timePeriodAttribute: {
-				Description: "Time period in which schedule will be active. If specified along with periodicity, this will act as the boundary of periodicity. Otherwise schedule action will be triggered at `start` time and terminate at `end` time.",
-				Type:        schema.TypeList,
-				Optional:    true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						startAttribute: {
-							Description:      "Time from which schedule will be active. Need to be in YYYY-MM-DD HH:mm:SS format. Eg 2006-01-02 15:04:05",
-							Type:             schema.TypeString,
-							Required:         true,
-							ValidateDiagFunc: dateValidation,
-						},
-						endAttribute: {
-							Description:      "Time until which schedule will be active. Need to be in YYYY-MM-DD HH:mm:SS format. Eg 2006-01-02 15:04:05",
-							Type:             schema.TypeString,
-							Required:         true,
-							ValidateDiagFunc: dateValidation,
-						},
-					},
-				},
-			},
-			periodicityAttribute: {
-				Description: "For defining periodic schedule. Periodic nature will be applicable from the time of creation of schedule, unless specific 'time_period' is specified",
-				Type:        schema.TypeList,
-				Optional:    true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						daysAttribute: {
-							Description:      "Days on which schedule need to be active. Comma separated values of `SUN`, `MON`, `TUE`, `WED`, `THU`, `FRI` and `SAT`. Eg : `MON,TUE,WED,THU,FRI` for Mon through Friday",
-							Type:             schema.TypeString,
-							Required:         true,
-							ValidateDiagFunc: daysValidationFunc,
-						},
-						startTimeAttribute: {
-							Description:      "Starting time of schedule action on the day. Accepted format is HH:MM. Eg : 13:15 for 01:15pm",
-							Type:             schema.TypeString,
-							Required:         true,
-							ValidateDiagFunc: timeValidation,
-						},
-						endTimeAttribute: {
-							Description:      "Ending time of schedule action on the day. Accepted format is HH:MM. Eg : 20:00 for 8pm",
-							Type:             schema.TypeString,
-							Required:         true,
-							ValidateDiagFunc: timeValidation,
-						},
-					},
-				},
-			},
-			rulesAttribute: {
-				Description: "ID of AutoStopping rules on which the schedule applies",
-				Required:    true,
-				Type:        schema.TypeList,
-				MinItems:    1,
-				Elem: &schema.Schema{
-					Type: schema.TypeFloat,
-				},
-			},
-		},
-	}
-
-	return resource
 }
 
 func resourceScheduleCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
