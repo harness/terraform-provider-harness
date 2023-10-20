bb76123986d8dde8251c6a0a07972ce74f8189ec
shalinlk <shalin.lk@harness.io>
2023-10-20 20:54:38 +0530

    Partial day downtime example - schedule of AutoStopping

--- examples/resources/harness_autostopping_schedule/resource.tf
+++ examples/resources/harness_autostopping_schedule/resource.tf
@@ -10,3 +10,15 @@ resource "harness_autostopping_schedule" "MondayWholeDayUp" {
     rules = [ 123 ]
 }
 
+resource "harness_autostopping_schedule" "MondayUpTill4:30pm" {
+    name = "MondayUpTill4:30pm"
+    schedule_type = "uptime"
+    time_zone = "UTC"    
+
+    repeats {
+        days = "MON"
+        end_time = "16:30"
+    }
+
+    rules = [ 123 ]
+}
