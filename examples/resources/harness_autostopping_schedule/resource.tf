ad152c099b8e8e4009230838451695e758b79f93
Shalin LK <shalin.lk@harness.io>
2023-10-25 17:14:21 +0530

    feat: [CCM-14715] Support for fixed schedule in Harness AutoStopping (#714)
    * Resource structure added for static schedule resource of AutoStopping
    
    * Added provision for specifying AutoStopping rule ID in schedules of AutoStopping rules
    
    * Parsing logic for AutoStopping static schedule
    
    * Introduced read context for schedule
    
    * Wired read context for read of static schedule of AutoStopping
    
    * Wired create context for static schedules of AutoStopping rules
    
    * Wired delete context for fixed schedules of AutoStopping rules
    
    * Update context wired for fixed schedules of AutoStopping rules
    
    * Added resource data file for fixed schedule of AutoStopping:
    
    * Wired resource provider and data source for schedule of Harness AutoStopping rule
    
    * Corrections on computed attribute in schedules of AutoStopping
    
    * Corrections - Schedules of AutoStopping
    
    * First tested version of schedule of AutoStopping rules
    
    * Final working version of Fixed schedule of AutoStopping rule
    
    * Test file for resource of fixed schedule of AutoStopping feature
    
    * made period end time to be optional in fixed schedule of AutoStopping
    
    * Correction - simplified structure of schedule of AutoStopping rule
    
    * Added example resource file for schedule of AutoStopping
    
    * Partial day downtime example - schedule of AutoStopping
    
    * Periodic schedule example - schedule of AutoStopping
    
    * Example of periodic schedule with starting date - schedule of AutoStopping
    
    * Example of periodic schedule with bounding date - schedule of AutoStopping
    
    * Example of implicit end time - schedule of AutoStopping
    
    * Example of whole day schedule - schedule of AutoStopping
    
    * Example of one time trigger schedule- schedule of AutoStopping
    
    * Example of overnight schedule- schedule of AutoStopping
    
    * Modified example name for clarity
    
    * Data source example file for schedule of AutoStopping rule
    
    * Docs for fixed schedules
    
    * Changelog for fixed schedule support of AutoStopping
    
    * GoSDK version upgraded to latest release of 0.3.56
    
    Source : https://harness.slack.com/archives/C0413DEHL1M/p1698216446558869?thread_ts=1698215288.389389&cid=C0413DEHL1M
    
    * Subcategory correction
    
    * Upgraded go sdk version to latest of v0.3.57
    
    source https://harness.slack.com/archives/C0413DEHL1M/p1698222960525819
    
    * Extracted value of time.DateTime to variable as the value is not supported in old Go version. This was causing build failure in pipeline

---
+++ examples/resources/harness_autostopping_schedule/resource.tf
@@ -0,0 +1,120 @@
+resource "harness_autostopping_schedule" "MondayWholeDayUp" {
+  name          = "MondayWholeDayUp"
+  schedule_type = "uptime"
+  time_zone     = "UTC"
+
+  repeats {
+    days = "MON"
+  }
+
+  rules = [123]
+}
+
+resource "harness_autostopping_schedule" "MondayUpTill4:30pm" {
+  name          = "MondayUpTill4:30pm"
+  schedule_type = "uptime"
+  time_zone     = "UTC"
+
+  repeats {
+    days     = "MON"
+    end_time = "16:30"
+  }
+
+  rules = [123]
+}
+
+resource "harness_autostopping_schedule" "MondayThroughFridayUptimeFrom9amTo6pm" {
+  name          = "MondayThroughFridayUptimeFrom9amTo6pm"
+  schedule_type = "uptime"
+  time_zone     = "UTC"
+
+  repeats {
+    days       = "MON, TUE, WED, THU, FRI"
+    start_time = "09:00"
+    end_time   = "18:00"
+  }
+
+  rules = [123]
+}
+
+resource "harness_autostopping_schedule" "MondayThroughFridayUptimeFrom9amTo6pmStartingFromDate" {
+  name          = "MondayThroughFridayUptimeFrom9amTo6pmStartingFrom"
+  schedule_type = "uptime"
+  time_zone     = "UTC"
+
+  starting_from = "2023-01-02 15:04:05"
+
+  repeats {
+    days       = "MON, TUE, WED, THU, FRI"
+    start_time = "09:00"
+    end_time   = "18:00"
+  }
+
+  rules = [123]
+}
+
+resource "harness_autostopping_schedule" "DownTimeForISTLuchOnEveryFridayInBetweenDate" {
+  name          = "DownTimeForISTLuchOnEveryFridayInBetweenDate"
+  schedule_type = "downtime"
+  time_zone     = "Asia/Calcutta"
+
+  starting_from = "2023-01-02 15:04:05"
+  ending_on     = "2023-02-02 15:04:05"
+
+  repeats {
+    days       = "FRI"
+    start_time = "12:30"
+    end_time   = "14:30"
+  }
+
+  rules = [123]
+}
+
+resource "harness_autostopping_schedule" "DowntimeEveryFridayAfterNoonTillEOD" {
+  name          = "DowntimeEveryFridayAfterNoonTillEOD"
+  schedule_type = "downtime"
+  time_zone     = "Asia/Calcutta"
+
+  repeats {
+    days       = "FRI"
+    start_time = "17:30"
+  }
+
+  rules = [123]
+}
+
+resource "harness_autostopping_schedule" "CompleteDownTimeOnWeekEnd" {
+  name          = "CompleteDownTimeOnWeekEnd"
+  schedule_type = "downtime"
+  time_zone     = "UTC"
+
+  repeats {
+    days = "SUN, SAT"
+  }
+
+  rules = [123]
+}
+
+resource "harness_autostopping_schedule" "TriggerDowntimeOnceOnDate" {
+  name          = "TriggerDowntimeOnceOnDate"
+  schedule_type = "downtime"
+  time_zone     = "UTC"
+
+  starting_from = "2023-01-02 15:04:05"
+
+  rules = [123]
+}
+
+resource "harness_autostopping_schedule" "OverNightDowntimeOnWeekDays" {
+  name          = "OverNightDowntimeOnWeekDays"
+  schedule_type = "downtime"
+  time_zone     = "Asia/Calcutta"
+
+  repeats {
+    days       = "MON, TUE, WED, THU, FRI"
+    start_time = "18:00"
+    end_time   = "07:00"
+  }
+
+  rules = [123]
+}
---
+++ internal/service/platform/autostopping/schedule/schedule_data_source.go
@@ -0,0 +1,80 @@
+package schedule
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func DataSourceFixedSchedule() *schema.Resource {
+	resource := &schema.Resource{
+		Description: "Data source for retrieving a fixed schedule for Harness AutoStopping rule",
+		ReadContext: resourceScheduleRead,
+		Schema: map[string]*schema.Schema{
+			"identifier": {
+				Description: "Unique identifier of the schedule",
+				Type:        schema.TypeFloat,
+				Computed:    true,
+			},
+			nameAttribute: {
+				Description: "Name of the schedule",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			scheduleTypeAttribute: {
+				Description: fmt.Sprintf("Type of the schedule. Valid values are `%s` and `%s`", uptimeSchedule, downtimeSchedule),
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			timeZoneAttribute: {
+				Description: "Time zone in which schedule needs to be executed",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			startingFromAttribute: {
+				Description: "Time from which schedule will be active. Schedule will take immediate effect if starting_from is not specified. Need to be in YYYY-MM-DD HH:mm:SS format. Eg 2006-01-02 15:04:05",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			endingOnAttribute: {
+				Description: "Time until which schedule will be active. Need to be in YYYY-MM-DD HH:mm:SS format. Eg 2006-01-02 15:04:05",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			repetitionAttribute: {
+				Description: "For defining periodic schedule. Periodic nature will be applicable from the time of creation of schedule, unless specific 'time_period' is specified",
+				Type:        schema.TypeList,
+				Computed:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						daysAttribute: {
+							Description: "Days on which schedule need to be active. Comma separated values of `SUN`, `MON`, `TUE`, `WED`, `THU`, `FRI` and `SAT`. Eg : `MON,TUE,WED,THU,FRI` for Mon through Friday",
+							Type:        schema.TypeString,
+							Computed:    true,
+						},
+						startTimeAttribute: {
+							Description: "Starting time of schedule action on the day. Accepted format is HH:MM. Eg : 13:15 for 01:15pm",
+							Type:        schema.TypeString,
+							Computed:    true,
+						},
+						endTimeAttribute: {
+							Description: "Ending time of schedule action on the day. Accepted format is HH:MM. Eg : 20:00 for 8pm",
+							Type:        schema.TypeString,
+							Computed:    true,
+						},
+					},
+				},
+			},
+			rulesAttribute: {
+				Description: "ID of AutoStopping rules on which the schedule applies",
+				Type:        schema.TypeList,
+				Computed:    true,
+				Elem: &schema.Schema{
+					Type: schema.TypeFloat,
+				},
+			},
+		},
+	}
+
+	return resource
+}
