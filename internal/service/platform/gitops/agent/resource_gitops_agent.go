6f5c6d750e55325e84ef15ee0736fdd7af57057f
Manavjot Singh <manavjot.singh@harness.io>
2023-11-13 14:42:46 -0500

    fix: [CDS-82842]: GitOps documentation improvements

--- internal/service/platform/gitops/agent/resource_gitops_agent.go
+++ internal/service/platform/gitops/agent/resource_gitops_agent.go
@@ -15,7 +15,7 @@ import (
 
 func ResourceGitopsAgent() *schema.Resource {
 	resource := &schema.Resource{
-		Description: "Resource for creating a Harness Gitops Agents.",
+		Description: "Resource for creating a Harness Gitops Agent.",
 
 		CreateContext: resourceGitopsAgentCreate,
 		ReadContext:   resourceGitopsAgentRead,
@@ -74,12 +74,12 @@ func ResourceGitopsAgent() *schema.Resource {
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"namespace": {
-							Description: "The k8s namespace that this agent resides in.",
+							Description: "The kubernetes namespace where the agent is installed.",
 							Type:        schema.TypeString,
 							Optional:    true,
 						},
 						"high_availability": {
-							Description: "Indicates if the deployment should be deployed using the deploy-ha.yaml",
+							Description: "Indicates if the agent is installed in HA mode.",
 							Type:        schema.TypeBool,
 							Optional:    true,
 						},
