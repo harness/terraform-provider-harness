94629f89d4e9c9afe81ffcdacf6f7be4f6d73943
Tejas Tiwari <tejas.tiwari@harness.io>
2023-11-13 14:42:35 +0530

    Added additional_metadata in the terraform spec

--- internal/service/platform/secret/text.go
+++ internal/service/platform/secret/text.go
@@ -38,6 +38,29 @@ func ResourceSecretText() *schema.Resource {
 				Type:        schema.TypeString,
 				Required:    true,
 			},
+			"additional_metadata": {
+				Description: "Additional Metadata for the Secret",
+				Type:        schema.TypeList,
+				Optional:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"values": {
+							Type:     schema.TypeSet,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"version": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									// Add other fields for the inner map as needed
+								},
+							},
+						},
+						// Add other fields for the outer map as needed
+					},
+				},
+			},
 		},
 	}
 	helpers.SetMultiLevelResourceSchema(resource.Schema)
@@ -91,6 +114,10 @@ func buildSecretText(d *schema.ResourceData) *nextgen.Secret {
 		secret.Text.Value = attr.(string)
 	}
 
+	if attr, ok := d.GetOk("additional_metadata"); ok {
+		secret.Text.AdditionalMetadata = readAdditionalMetadata(attr)
+	}
+
 	return secret
 }
 
@@ -105,3 +132,30 @@ func readSecretText(d *schema.ResourceData, secret *nextgen.Secret) error {
 	}
 	return nil
 }
+
+func readAdditionalMetadata(metadata interface{}) nextgen.AdditionalMetadata {
+	result := nextgen.AdditionalMetadata{}
+
+	// Check if additional_metadata block is present
+	metadataList := metadata.([]interface{})
+	if len(metadataList) > 0 {
+		metadataMap := metadataList[0].(map[string]interface{})
+
+		if valuesSet, ok := metadataMap["values"].(*schema.Set); ok {
+			result.Values = make(map[string]string)
+
+			// Loop through "values" set
+			for _, v := range valuesSet.List() {
+				valueMap := v.(map[string]interface{})
+				if version, ok := valueMap["version"].(string); ok {
+					result.Values["version"] = version
+				}
+				// Add other fields as needed
+			}
+		}
+	}
+
+	return result
+}
+
+// This code uses the Terraform SDK's schema package to decode the additional_metadata parameter from the Terraform configuration into the AdditionalMetadata struct. Adjust the import paths and modify the decoding logic based on your actual requirements and structure.
