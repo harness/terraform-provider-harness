5152e148db2e755e734cf4e43560c23b7950d80f
Mirko Teodorovic <mirko.teodorovic@harness.io>
2023-11-15 16:46:02 +0100

    feat: [CDS-84451]: add proxy config
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@harness.io>

--- internal/service/platform/gitops/repository_certificates/data_source_gitops_repo_cert_test.go
+++ internal/service/platform/gitops/repository_certificates/data_source_gitops_repo_cert_test.go
@@ -31,6 +31,12 @@ func TestAccDataSourceGitOpsRepoCert(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "account_id", accountId),
 					resource.TestCheckResourceAttr(resourceName, "agent_id", agentId),
 				),
+			}, {
+				Config: testAccDataSourceRepoCertSSH(id, accountId, name, agentId, clusterName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountId),
+					resource.TestCheckResourceAttr(resourceName, "agent_id", agentId),
+				),
 			},
 		},
 	})
@@ -52,6 +58,41 @@ func testAccDataSourceRepoCert(id string, accountId string, name string, agentId
 						server_name = "rajRepoCert"
 						cert_type = "https"
 						cert_data = "LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUZsakNDQTM0Q0NRRDlweEZoeFdmb0tUQU5CZ2txaGtpRzl3MEJBUXNGQURDQmpERUxNQWtHQTFVRUJoTUMKU1U0eEZEQVNCZ05WQkFnTUMwMUJTRUZTUVZOSVZGSkJNUTB3Q3dZRFZRUUhEQVJRVlU1Rk1SQXdEZ1lEVlFRSwpEQWRJUVZKT1JWTlRNUTh3RFFZRFZRUUxEQVpFUlZaUFVGTXhFVEFQQmdOVkJBTU1DR0pzWVdoaWJHRm9NU0l3CklBWUpLb1pJaHZjTkFRa0JGaE55WW1GMmFYTnJZWEpBWjIxaGFXd3VZMjl0TUI0WERUSXlNVEF5TVRJeE5ESTAKTjFvWERUSXpNVEF5TVRJeE5ESTBOMW93Z1l3eEN6QUpCZ05WQkFZVEFrbE9NUlF3RWdZRFZRUUlEQXROUVVoQgpVa0ZUU0ZSU1FURU5NQXNHQTFVRUJ3d0VVRlZPUlRFUU1BNEdBMVVFQ2d3SFNFRlNUa1ZUVXpFUE1BMEdBMVVFCkN3d0dSRVZXVDFCVE1SRXdEd1lEVlFRRERBaGliR0ZvWW14aGFERWlNQ0FHQ1NxR1NJYjNEUUVKQVJZVGNtSmgKZG1semEyRnlRR2R0WVdsc0xtTnZiVENDQWlJd0RRWUpLb1pJaHZjTkFRRUJCUUFEZ2dJUEFEQ0NBZ29DZ2dJQgpBT0IrdFZUUmxTcGdNMndaYWtnc2FBN0lTczZ3VmR1YXBpYlV0MXZNZEtjZklVTDJXNUhzK1pRWW9nZHcyK1VmCmVWR2ZZTUZyS3prb2hkWm5Ra2lxTjN3UkVMUGs2bHNqbDd0ZlFrcFpTODcvSklGZjhobytRZWl4am9rdnhrTGkKL0U0UldaR3lxQU1TQ2syNWcvNVpxUzhjdklLaEphVUx5Mm9KaWN3Zm5QUUZlbWpPdG5PV2tTS09ZWk1wY0cxYwpLVzRRQlMwTjdsemJuR05PVjYvcUJ5YkdzQytJa3hORmNBZUNhMHVxMFJSNWNVZThvNm9WS3M3SGwwUVFQTU9qCnpEaEtJc01rWUlETS8zNFEvaG4rQVB2eVNuL3M3UFpVVWNIcE9zUmVTcE1HdVQwcjJ4SmVCRGZXbWVYck1YRDMKRWpwOUlxVGp3TktOcnhZR2RFUytQSDE1TUpIMEk4R2lvV2RyTTE3T3lKbk0ray9odmNyNm1lRmduWXVlb21PVQpsQ0dPUkkvRGM0bExuVGZFdldVUVg1eGNaNEUxdkU4anUyR0srRGVQM3RuT0NJY2ZrZ1hVR0tXRTlCNFMyTHpECnVOSW1hVllNb3ZKM3VYTjQ1MHFiY1ZhMHNhZWZyQjA3TzBwbmo5Mm0vSzJLUGpsRzRYY2hzdU11ZmpyQitOZDgKTHhGL1FjMkZFVHc0bnZGNDRUTGoyTmlrTzdML1JwZVdUMGVTeVZJZUZWdzQ1WmJrYkQraGpUV1lmdHVCOEF5VwpPemhDZ1RhODhDbzVEbzd5UVkzdFNmYm1tWCtBd0hYRDdzS0tzRk03aG9hWEF4Mjl0TGVma04wWGpIcUpXUWgyCkdtTDhCUW1tVVY2dVZkMGRGdUlBSmtKVE5RQXNwVSs0Q2hmS1J2bkdVODVuQWdNQkFBRXdEUVlKS29aSWh2Y04KQVFFTEJRQURnZ0lCQUI2b01MSzFTSFVMbzJ6ditwWk9pbDRlcm95VEcxUUU4Q04wU1haRWRESy9SbkNFaCtNeQpETldOSUxrTG1MZnQydGUzbnFXN3VjVlZhcG9IWFBOS09HRHR2UjlBVEFiSEN3UEF4TUxkT21CZ0R1dFI2dHpBCmNHNklZemU0VVp0NTJ0dTJtRk42REp3aXVoNWRPcUlKK0pYK0tUS2tDYktPZlNOdXhoUlFZVFZ0dHR1NklTQWwKK2ppR0ozYkdSUktkaUxKUWIvVWFubHhValZmVW1wdUIvV2RmZzBES0oxUkpDdW1XbkJFbDF5RXUyak9SUnZqKwpQTEdtNFhOU1A3YVhwSzhBS0p2ZkJaMmxiNGhIa2JrQUxiVys0OTMvWkZRV1orZ0NRQ1MvUEtaWThyYkVBTkZPCnpWMVdzTms0WWdSaE5rYllFNkV2eDBzNXV1MnovZTZIeVRrR25tdXdrSVNIcjJsUGRrS2tFYjJrbTZsUG4zYnoKRkNpNFl4Vy8vYzBJeDdZaHQ5MnNDLzAyUisyd0cvSTlWMW1zLzFhOHAzUGhvOHB6aEdFMGdzcENFMEIyRHd6aQovajdudEVCZ000SGFmcEJGMDRETEYyUjNkQTJUSmNWQWpXdlZLUkhTbkhvNmlMaklvaXppQUxobHlDbHFQWVhnClI5c2JJWWtBamg3SnRTaVlkTGVjeHVNaU5XU0xUd1R5dk56ck5LQ3hvWlhiSE14N1dlY0VTTUZIM3NQdkJLOVAKSUFuVGVoRTFrU1FLZmtBVmt2TWVaQnRaU2JIdStORU9MRmJsaUhwallGMHhmQjdOTnV1Vy9uYk9XeXA0N1NQNgpKTysvZ2llMVhRb2thVloyUG9sR3pmVHpTV1c5V1NROUpEMjRjUXBFczIzVTBsWlV2UWsvcXdyRQotLS0tLUVORCBDRVJUSUZJQ0FURS0tLS0t"
+					}
+				}
+			}
+		}
+
+		data "harness_platform_gitops_repo_cert" "test" {
+			depends_on = [harness_platform_gitops_repo_cert.test]
+			account_id = "%[2]s"
+			agent_id = "%[4]s"
+		}
+`, id, accountId, name, agentId, clusterName)
+}
+
+// bitbucket.org ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAubiN81eDcafrgMeLzaFPsw2kNvEcqTKl/VqLat/MaB33pZy0y3rJZtnqwR2qOOvbwKZYKiEO1O6VqNEBxKvJJelCq0dTXWT5pbO2gDXC6h6QDXCaHo6pOHGPUy+YBaGQRGuSusMEASYiWunYN0vCAI8QaXnWMXNMdFP3jHAJH0eDsoiGnLPBlBp4TNm6rYI74nMzgz3B9IikW4WVK+dc8KZJZWYjAuORU3jc1c/NPskD2ASinf8v3xnfXeukU0sJ5N6m5E8VLjObPEO+mN2t/FZTMZLiFqPWc/ALSqnMnnhwrNi2rbfg/rd/IpL8Le3pSBne8+seeFVBoGqzHM9yXw==
+// "serverName":"bitbucket.org",
+// "certType":"ssh",
+// "certSubType": "ssh-rsa",
+// "certData":"QUFBQUIzTnphQzF5YzJFQUFBQUJJd0FBQVFFQXViaU44MWVEY2FmcmdNZUx6YUZQc3cya052RWNxVEtsL1ZxTGF0L01hQjMzcFp5MHkzckpadG5xd1IycU9PdmJ3S1pZS2lFTzFPNlZxTkVCeEt2SkplbENxMGRUWFdUNXBiTzJnRFhDNmg2UURYQ2FIbzZwT0hHUFV5K1lCYUdRUkd1U3VzTUVBU1lpV3VuWU4wdkNBSThRYVhuV01YTk1kRlAzakhBSkgwZURzb2lHbkxQQmxCcDRUTm02cllJNzRuTXpnejNCOUlpa1c0V1ZLK2RjOEtaSlpXWWpBdU9SVTNqYzFjL05Qc2tEMkFTaW5mOHYzeG5mWGV1a1Uwc0o1TjZtNUU4VkxqT2JQRU8rbU4ydC9GWlRNWkxpRnFQV2MvQUxTcW5Nbm5od3JOaTJyYmZnL3JkL0lwTDhMZTNwU0JuZTgrc2VlRlZCb0dxekhNOXlYdz09","certInfo":""}]},"upsert":true}' \
+func testAccDataSourceRepoCertSSH(id string, accountId string, name string, agentId string, clusterName string) string {
+	return fmt.Sprintf(`
+		resource "harness_platform_gitops_repo_cert" "test" {
+			account_id = "%[2]s"
+			agent_id = "%[4]s"
+
+ 			request {
+				upsert = true
+				certificates {
+					metadata {
+
+					}
+					items {
+						server_name = "bitbucket.org"
+						cert_type = "ssh"
+                        cert_sub_type = "ssh-rsa"
+						cert_data = "QUFBQUIzTnphQzF5YzJFQUFBQUJJd0FBQVFFQXViaU44MWVEY2FmcmdNZUx6YUZQc3cya052RWNxVEtsL1ZxTGF0L01hQjMzcFp5MHkzckpadG5xd1IycU9PdmJ3S1pZS2lFTzFPNlZxTkVCeEt2SkplbENxMGRUWFdUNXBiTzJnRFhDNmg2UURYQ2FIbzZwT0hHUFV5K1lCYUdRUkd1U3VzTUVBU1lpV3VuWU4wdkNBSThRYVhuV01YTk1kRlAzakhBSkgwZURzb2lHbkxQQmxCcDRUTm02cllJNzRuTXpnejNCOUlpa1c0V1ZLK2RjOEtaSlpXWWpBdU9SVTNqYzFjL05Qc2tEMkFTaW5mOHYzeG5mWGV1a1Uwc0o1TjZtNUU4VkxqT2JQRU8rbU4ydC9GWlRNWkxpRnFQV2MvQUxTcW5Nbm5od3JOaTJyYmZnL3JkL0lwTDhMZTNwU0JuZTgrc2VlRlZCb0dxekhNOXlYdz09"
 					}
 				}
 			}
