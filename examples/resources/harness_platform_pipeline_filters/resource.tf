e0b7bd237595a2cec1e70ef969cd0c53f2927f48
Manavjot Singh <manavjot.singh@harness.io>
2023-11-28 14:27:23 -0500

    updating the datasource

--- examples/resources/harness_platform_pipeline_filters/resource.tf
+++ examples/resources/harness_platform_pipeline_filters/resource.tf
@@ -5,10 +5,10 @@ resource "harness_platform_pipeline_filters" "test" {
   project_id = "project_id"
   type       = "PipelineSetup"
   filter_properties {
-    name = "pipeline_name"
-    description = "pipeline_description"
+    name                 = "pipeline_name"
+    description          = "pipeline_description"
     pipeline_identifiers = ["id1", "id2"]
-    filter_type = "PipelineSetup"
+    filter_type          = "PipelineSetup"
   }
   filter_visibility = "EveryOne"
 }
@@ -20,29 +20,29 @@ resource "harness_platform_pipeline_filters" "example_with_tags" {
   name       = "name"
   org_id     = "org_id"
   project_id = "project_id"
-  type = "PipelineSetup"
+  type       = "PipelineSetup"
   filter_properties {
-    filter_type = "PipelineSetup"
+    filter_type   = "PipelineSetup"
     pipeline_tags = [
       {
-        "key" = "tag1"
+        "key"   = "tag1"
         "value" = "123"
       },
       {
-        "key" = "tag2"
+        "key"   = "tag2"
         "value" = "456"
       },
     ]
     module_properties {
       cd {
-        deployment_types = "Kubernetes"
-        service_names = ["service1", "service2"]
-        environment_names = ["env1", "env2"]
+        deployment_types       = "Kubernetes"
+        service_names          = ["service1", "service2"]
+        environment_names      = ["env1", "env2"]
         artifact_display_names = ["artificatname1", "artifact2"]
       }
       ci {
         build_type = "branch"
-        branch = "branch123"
+        branch     = "branch123"
         repo_names = "repo1234"
       }
     }
