cc5f521b2895fd170b2915da08a39ee40ffd73b4
Tejas Tiwari <tejas.tiwari@harness.io>
2024-01-02 10:21:18 +0530

    feat: [PL-42336]: Added docs for additional_metadata in referenced secrets (#823)

--- examples/resources/harness_platform_secret_text/resource.tf
+++ examples/resources/harness_platform_secret_text/resource.tf
@@ -19,3 +19,20 @@ resource "harness_platform_secret_text" "reference" {
   value_type                = "Reference"
   value                     = "secret"
 }
+
+resource "harness_platform_secret_text" "gcp_secret_manager_reference" {
+  identifier  = "identifier"
+  name        = "name"
+  description = "example"
+  tags        = ["foo:bar"]
+
+  secret_manager_identifier = "gcpSecretManager"
+  value_type                = "Reference"
+  value                     = "secret"
+
+  additional_metadata { 
+    values {
+      version = "1"
+    }
+  }
+}
