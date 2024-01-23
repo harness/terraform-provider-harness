6673e53d20b467888f1fdd92f9fef1a149258d1a
Imran Khan <56226186+imrantechwiz@users.noreply.github.com>
2024-01-23 13:34:23 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-version=0.90.12
-source=registry.terraform.io/harness/harness
-platform=darwin_amd64
-mkdir -p ~/.terraform.d/plugins/$source/$version/$platform/
-echo "HI"
-cp terraform-provider-harness ~/.terraform.d/plugins/$source/$version/$platform/terraform-provider-harness
-echo "Done"
