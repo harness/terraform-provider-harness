aa545beb15d956e01cd0ea60935506e8d926c630
Darko Draskovic <darko.draskovic@harness.io>
2024-01-30 11:29:59 +0100

    Add complete buildRepo and readRepo func
    Signed-off-by: Darko Draskovic <darko.draskovic@harness.io>

--- internal/service/platform/repo/resource_repo.go
+++ internal/service/platform/repo/resource_repo.go
@@ -3,10 +3,9 @@ package repo
 import (
 	"context"
 
-	"github.com/harness/harness-go-sdk/harness/nextgen"
+	"github.com/harness/harness-go-sdk/harness/code"
 	"github.com/harness/terraform-provider-harness/helpers"
 	"github.com/harness/terraform-provider-harness/internal"
-	"github.com/harness/terraform-provider-harness/internal/utils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -22,42 +21,6 @@ func ResourceRepo() *schema.Resource {
 		Importer:      helpers.MultiLevelResourceImporter,
 
 		Schema: map[string]*schema.Schema{
-			"id": {
-				Description: "ID of the repository.",
-				Type:        schema.TypeInt,
-				Required:    true,
-			},
-			"version": {
-				Description: "Version of the repository.",
-				Type:        schema.TypeInt,
-				Required:    true,
-			},
-			"parent_id": {
-				Description: "ID of the parent repository.",
-				Type:        schema.TypeInt,
-				Required:    true,
-			},
-			"uid": {
-				Description: "UID of the repository.",
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-			},
-			"path": {
-				Description: "Path of the repository.",
-				Type:        schema.TypeString,
-				Required:    true,
-			},
-			"description": {
-				Description: "Description of the repository.",
-				Type:        schema.TypeString,
-				Optional:    true,
-			},
-			"is_public": {
-				Description: "Whether the repository is public.",
-				Type:        schema.TypeBool,
-				Required:    true,
-			},
 			"created_by": {
 				Description: "ID of the user who created the repository.",
 				Type:        schema.TypeInt,
@@ -66,6 +29,92 @@ func ResourceRepo() *schema.Resource {
 			"created": {
 				Description: "Timestamp when the repository was created.",
 				Type:        schema.TypeInt,
+				Optional:    true,
+				Computed:    true,
+			},
+			"default_branch": {
+				Description: "Default branch of the repository.",
+				Type:        schema.TypeString,
+				Optional:    true,
+			},
+			"description": {
+				Description: "Description of the repository.",
+				Type:        schema.TypeString,
+				Optional:    true,
+			},
+			"fork_id": {
+				Description: "ID of the forked repository.",
+				Type:        schema.TypeInt,
+				Optional:    true,
+			},
+			"git_url": {
+				Description: "Git URL of the repository.",
+				Type:        schema.TypeString,
+				Optional:    true,
+			},
+			"id": {
+				Description: "ID of the repository.",
+				Type:        schema.TypeInt,
+				Required:    true,
+			},
+			"importing": {
+				Description: "Whether the repository is being imported.",
+				Type:        schema.TypeBool,
+				Optional:    true,
+			},
+			"is_public": {
+				Description: "Whether the repository is public.",
+				Type:        schema.TypeBool,
+				Required:    true,
+			},
+			"num_closed_pulls": {
+				Description: "Number of closed pull requests.",
+				Type:        schema.TypeInt,
+				Optional:    true,
+			},
+			"num_forks": {
+				Description: "Number of forks.",
+				Type:        schema.TypeInt,
+				Optional:    true,
+			},
+			"num_merged_pulls": {
+				Description: "Number of merged pull requests.",
+				Type:        schema.TypeInt,
+				Optional:    true,
+			},
+			"num_open_pulls": {
+				Description: "Number of open pull requests.",
+				Type:        schema.TypeInt,
+				Optional:    true,
+			},
+			"num_pulls": {
+				Description: "Total number of pull requests.",
+				Type:        schema.TypeInt,
+				Optional:    true,
+			},
+			"parent_id": {
+				Description: "ID of the parent repository.",
+				Type:        schema.TypeInt,
+				Required:    true,
+			},
+			"path": {
+				Description: "Path of the repository.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"size": {
+				Description: "Size of the repository.",
+				Type:        schema.TypeInt,
+				Optional:    true,
+			},
+			"size_updated": {
+				Description: "Timestamp when the repository size was last updated.",
+				Type:        schema.TypeInt,
+				Optional:    true,
+			},
+			"uid": {
+				Description: "UID of the repository.",
+				Type:        schema.TypeString,
 				Optional:    true,
 				Computed:    true,
 			},
@@ -84,46 +133,77 @@ func ResourceRepo() *schema.Resource {
 }
 
 func resourceRepoRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	c, ctx := meta.(*internal.Session).GetPlatformClientWithContext(ctx)
+	c, ctx := meta.(*internal.Session).GetCodeClientWithContext(ctx)
 	id := d.Id()
-	_, _ = c, id
+
+	repo, resp, err := c.RepositoryApi.FindRepository(ctx, id)
+	if err != nil {
+		return helpers.HandleReadApiError(err, d, resp)
+	}
+
+	readRepo(d, &repo)
 
 	return nil
 }
 
 func resourceRepoCreateOrUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	c, ctx := meta.(*internal.Session).GetPlatformClientWithContext(ctx)
+	c, ctx := meta.(*internal.Session).GetCodeClientWithContext(ctx)
 	_ = c
 
 	return nil
 }
 
 func resourceRepoDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	c, ctx := meta.(*internal.Session).GetPlatformClientWithContext(ctx)
+	c, ctx := meta.(*internal.Session).GetCodeClientWithContext(ctx)
 	_ = c
 
 	return nil
 }
 
-func buildRepo(d *schema.ResourceData) *nextgen.Project {
-	return &nextgen.Project{
-		Identifier:    d.Get("identifier").(string),
-		OrgIdentifier: d.Get("org_id").(string),
-		Name:          d.Get("name").(string),
-		Color:         d.Get("color").(string),
-		Description:   d.Get("description").(string),
-		Modules:       utils.InterfaceSliceToStringSlice(d.Get("modules").(*schema.Set).List()),
-		Tags:          helpers.ExpandTags(d.Get("tags").(*schema.Set).List()),
+func buildRepo(d *schema.ResourceData) *code.TypesRepository {
+	return &code.TypesRepository{
+		Created:        d.Get("created").(int32),
+		CreatedBy:      d.Get("created_by").(int32),
+		DefaultBranch:  d.Get("default_branch").(string),
+		Description:    d.Get("description").(string),
+		ForkId:         d.Get("fork_id").(int32),
+		GitUrl:         d.Get("git_url").(string),
+		Id:             d.Get("id").(int32),
+		Importing:      d.Get("importing").(bool),
+		IsPublic:       d.Get("is_public").(bool),
+		NumClosedPulls: d.Get("num_closed_pulls").(int32),
+		NumForks:       d.Get("num_forks").(int32),
+		NumMergedPulls: d.Get("num_merged_pulls").(int32),
+		NumOpenPulls:   d.Get("num_open_pulls").(int32),
+		NumPulls:       d.Get("num_pulls").(int32),
+		ParentId:       d.Get("parent_id").(int32),
+		Path:           d.Get("path").(string),
+		Size:           d.Get("size").(int32),
+		SizeUpdated:    d.Get("size_updated").(int32),
+		Uid:            d.Get("uid").(string),
+		Updated:        d.Get("updated").(int32),
 	}
 }
 
-// func readRepo(d *schema.ResourceData, Repo *nextgen.Repo) {
-// 	d.SetId(Repo.Identifier)
-// 	d.Set("identifier", Repo.Identifier)
-// 	d.Set("org_id", Repo.OrgIdentifier)
-// 	d.Set("name", Repo.Name)
-// 	d.Set("color", Repo.Color)
-// 	d.Set("description", Repo.Description)
-// 	d.Set("modules", Repo.Modules)
-// 	d.Set("tags", helpers.FlattenTags(Repo.Tags))
-// }
+func readRepo(d *schema.ResourceData, resp *code.TypesRepository) {
+	d.Set("created", resp.Created)
+	d.Set("created_by", resp.CreatedBy)
+	d.Set("default_branch", resp.DefaultBranch)
+	d.Set("description", resp.Description)
+	d.Set("fork_id", resp.ForkId)
+	d.Set("git_url", resp.GitUrl)
+	d.Set("id", resp.Id)
+	d.Set("importing", resp.Importing)
+	d.Set("is_public", resp.IsPublic)
+	d.Set("num_closed_pulls", resp.NumClosedPulls)
+	d.Set("num_forks", resp.NumForks)
+	d.Set("num_merged_pulls", resp.NumMergedPulls)
+	d.Set("num_open_pulls", resp.NumOpenPulls)
+	d.Set("num_pulls", resp.NumPulls)
+	d.Set("parent_id", resp.ParentId)
+	d.Set("path", resp.Path)
+	d.Set("size", resp.Size)
+	d.Set("size_updated", resp.SizeUpdated)
+	d.Set("uid", resp.Uid)
+	d.Set("updated", resp.Updated)
+}
--- internal/session.go
+++ internal/session.go
@@ -4,17 +4,19 @@ import (
 	"context"
 
 	"github.com/harness/harness-go-sdk/harness/cd"
+	"github.com/harness/harness-go-sdk/harness/code"
 	"github.com/harness/harness-go-sdk/harness/nextgen"
 	"github.com/harness/harness-go-sdk/harness/policymgmt"
 	openapi_client_nextgen "github.com/harness/harness-openapi-go-client/nextgen"
 )
 
 type Session struct {
-	AccountId string
-	Endpoint  string
-	CDClient  *cd.ApiClient
-	PLClient  *nextgen.APIClient
-	Client    *openapi_client_nextgen.APIClient
+	AccountId  string
+	Endpoint   string
+	CDClient   *cd.ApiClient
+	PLClient   *nextgen.APIClient
+	Client     *openapi_client_nextgen.APIClient
+	CodeClient code.APIClient
 }
 
 func (s *Session) GetPlatformClient() (*nextgen.APIClient, context.Context) {
@@ -43,4 +45,13 @@ func (s *Session) GetPolicyManagementClient() *policymgmt.APIClient {
 	)
 	c.ChangeBasePath(s.Endpoint + "/pm")
 	return c
+}
+
+func (s *Session) GetCodeClientWithContext(ctx context.Context) (*code.APIClient, context.Context) {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	cfg := code.NewConfiguration()
+	return code.NewAPIClient(cfg), ctx
 }
