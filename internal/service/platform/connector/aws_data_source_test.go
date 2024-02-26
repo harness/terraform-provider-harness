118372fc47f053025f8cb9549b15614517aaa3b6
Pragyesh Mishra <pragyesh.mishra@harness.io>
2024-02-26 12:39:31 +0530

    [feat]: [CDS-83950]: add oidc auth to aws connector

--- internal/service/platform/connector/aws_data_source_test.go
+++ internal/service/platform/connector/aws_data_source_test.go
@@ -36,6 +36,34 @@ func TestAccDataSourceConnectorAws(t *testing.T) {
 	})
 }
 
+func TestOidcDataSourceConnectorAws(t *testing.T) {
+
+	var (
+		name         = fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(4))
+		resourceName = "data.harness_platform_connector_aws.test"
+	)
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceConnectorAws(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", name),
+					resource.TestCheckResourceAttr(resourceName, "identifier", name),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "description", "test"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "oidc_authentication.0.iam_role_arn", "test"),
+					resource.TestCheckResourceAttr(resourceName, "oidc_authentication.0.delegate_selectors.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "oidc_authentication.0.region", "us-east-1"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccDataSourceConnectorAwsFullJitterBackOff(t *testing.T) {
 
 	var (
@@ -135,6 +163,27 @@ func testAccDataSourceConnectorAws(name string) string {
 			tags = ["foo:bar"]
 
 			inherit_from_delegate {
+				delegate_selectors = ["harness-delegate"]
+				region = "us-east-1"
+			}
+		}
+
+		data "harness_platform_connector_aws" "test" {
+			identifier = harness_platform_connector_aws.test.identifier
+		}
+	`, name)
+}
+
+func testOidcDataSourceConnectorAws(name string) string {
+	return fmt.Sprintf(`
+		resource "harness_platform_connector_aws" "test" {
+			identifier = "%[1]s"
+			name = "%[1]s"
+			description = "test"
+			tags = ["foo:bar"]
+
+			oidc_authentication {
+				iam_role_arn = "test"
 				delegate_selectors = ["harness-delegate"]
 				region = "us-east-1"
 			}
