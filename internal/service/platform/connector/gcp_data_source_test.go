90d45874744704021d8f554fdd62f8a9804b89a1
Tarun Uba <tarun.uba@harness.io>
2024-07-30 14:23:15 +0530

    adding tests

--- internal/service/platform/connector/gcp_data_source_test.go
+++ internal/service/platform/connector/gcp_data_source_test.go
@@ -34,6 +34,32 @@ func TestAccDataSourceConnectorGcp(t *testing.T) {
 	})
 }
 
+func TestOidcDataSourceConnectorGcp(t *testing.T) {
+
+	var (
+		name         = fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(4))
+		resourceName = "data.harness_platform_connector_gcp.test"
+	)
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testOidcDataSourceConnectorGcp(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", name),
+					resource.TestCheckResourceAttr(resourceName, "identifier", name),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "description", "test"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "oidc_authentication.0.delegate_selectors.#", "1"),
+				),
+			},
+		},
+	})
+}
+
 func testAccDataSourceConnectorGcp(name string) string {
 	return fmt.Sprintf(`
 		resource "harness_platform_connector_gcp" "test" {
@@ -45,6 +71,28 @@ func testAccDataSourceConnectorGcp(name string) string {
 			inherit_from_delegate {
 				delegate_selectors = ["harness-delegate"]
 			}
+		}
+
+		data "harness_platform_connector_gcp" "test" {
+			identifier = harness_platform_connector_gcp.test.identifier
+		}
+	`, name)
+}
+
+func testOidcDataSourceConnectorGcp(name string) string {
+	return fmt.Sprintf(`
+		resource "harness_platform_connector_gcp" "test" {
+			identifier = "%[1]s"
+			name = "%[1]s"
+			description = "test"
+			tags = ["foo:bar"]
+
+			oidc_authentication {
+				workload_pool_id = "harness-pool-test"
+				provider_id = "harness"
+				gcp_project_id = "1234567"
+				service_account_email = "harness.sample.iam.gserviceaccount.com"
+				delegate_selectors = ["harness-delegate"]
 		}
 
 		data "harness_platform_connector_gcp" "test" {
