1d8e7ba1a1603ffb6fdb23a56536d28dc076a24b
Mirko Teodorovic <mirko.teodorovic@harness.io>
2024-08-29 14:28:12 +0200

    add handling for gitops errors
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@harness.io>

--- helpers/errors.go
+++ helpers/errors.go
@@ -1,13 +1,12 @@
 package helpers
 
 import (
-	"encoding/json"
+	"github.com/harness/harness-go-sdk/harness/nextgen"
+	openapi_client_nextgen "github.com/harness/harness-openapi-go-client/nextgen"
 	"google.golang.org/grpc/codes"
 	"net/http"
 
 	"github.com/harness/harness-go-sdk/harness/dbops"
-	"github.com/harness/harness-go-sdk/harness/nextgen"
-	openapi_client_nextgen "github.com/harness/harness-openapi-go-client/nextgen"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -34,32 +33,6 @@ func HandleDBOpsApiError(err error, d *schema.ResourceData, httpResp *http.Respo
 		}
 	}
 
-	return diag.Errorf(err.Error())
-}
-
-func HandleReadApiError(err error, d *schema.ResourceData, httpResp *http.Response) diag.Diagnostics {
-	return handleApiError(err, d, httpResp, true)
-}
-
-func HandleDBOpsReadApiError(err error, d *schema.ResourceData, httpResp *http.Response) diag.Diagnostics {
-	_, ok := err.(dbops.GenericSwaggerError)
-	if ok && httpResp != nil {
-		if httpResp.StatusCode == 401 {
-			return diag.Errorf(httpResp.Status + "\n" + "Hint:\n" +
-				"1) Please check if token has expired or is wrong.\n" +
-				"2) Harness Provider is misconfigured. For firstgen resources please give the correct api_key and for nextgen resources please give the correct platform_api_key.")
-		}
-		if httpResp.StatusCode == 403 {
-			return diag.Errorf(httpResp.Status + "\n" + "Hint:\n" +
-				"1) Please check if the token has required permission for this operation.\n" +
-				"2) Please check if the token has expired or is wrong.")
-		}
-		if httpResp.StatusCode == 404 {
-			d.SetId("")
-			d.MarkNewResource()
-			return nil
-		}
-	}
 	return diag.Errorf(err.Error())
 }
 
@@ -126,5 +99,31 @@ func handleApiError(err error, d *schema.ResourceData, httpResp *http.Response, 
 		return diag.Errorf(err_openapi_client.Error())
 	}
 
+	return diag.Errorf(err.Error())
+}
+
+func HandleReadApiError(err error, d *schema.ResourceData, httpResp *http.Response) diag.Diagnostics {
+	return handleApiError(err, d, httpResp, true)
+}
+
+func HandleDBOpsReadApiError(err error, d *schema.ResourceData, httpResp *http.Response) diag.Diagnostics {
+	_, ok := err.(dbops.GenericSwaggerError)
+	if ok && httpResp != nil {
+		if httpResp.StatusCode == 401 {
+			return diag.Errorf(httpResp.Status + "\n" + "Hint:\n" +
+				"1) Please check if token has expired or is wrong.\n" +
+				"2) Harness Provider is misconfigured. For firstgen resources please give the correct api_key and for nextgen resources please give the correct platform_api_key.")
+		}
+		if httpResp.StatusCode == 403 {
+			return diag.Errorf(httpResp.Status + "\n" + "Hint:\n" +
+				"1) Please check if the token has required permission for this operation.\n" +
+				"2) Please check if the token has expired or is wrong.")
+		}
+		if httpResp.StatusCode == 404 {
+			d.SetId("")
+			d.MarkNewResource()
+			return nil
+		}
+	}
 	return diag.Errorf(err.Error())
 }
