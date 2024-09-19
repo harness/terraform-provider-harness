e0fc649db6d6dabe2be1187db587b1fe66b34a85
Rajarshee Chatterjee <rajarshee.chatterjee@harness.io>
2024-09-19 14:41:52 +0530

    feat: [CCM-19504]: Terraform Support for Governance Rules

--- examples/resources/harness_governance_rule/resources.tf
+++ examples/resources/harness_governance_rule/resources.tf
@@ -3,5 +3,5 @@ resource "harness_governance_rule" "example" {
   name               = "name"
   cloud_provider     = "AWS/AZURE/GCP"
   description        = "description"
-  rules_yaml 		     = "policies:\n  - name: aws-list-ec2\n    resource: aws.ec2"
+  rules_yaml         = "policies:\n  - name: aws-list-ec2\n    resource: aws.ec2"
 }
