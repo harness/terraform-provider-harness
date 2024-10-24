858f96d658284895c81ab9c3f56e2ed9020887e0
Ritek Rounak <ritek.rounak@harness.io>
2024-10-25 00:07:55 +0530

    fix: [CDS-101927]: Add Cd Pre-requisite

---
+++ internal/service/cd_nextgen/_PreRequisites/main.tf
@@ -0,0 +1,21 @@
+# main.tf
+
+terraform {
+  required_providers {
+    harness = {
+      source = "harness/harness"
+    }
+  }
+}
+
+resource "harness_platform_secret_text" "TEST_spot_account_id" {
+  identifier                = "TEST_spot_account_id"
+  name                      = "TEST_spot_account_id"
+  description               = "This is a test Spot secret text"
+  tags                      = ["foo:bar"]
+  secret_manager_identifier = "harnessSecretManager"
+  value_type                = "Inline"
+  value                     = "my_secret_value"
+}
+
+# Additional resources can be added as needed
