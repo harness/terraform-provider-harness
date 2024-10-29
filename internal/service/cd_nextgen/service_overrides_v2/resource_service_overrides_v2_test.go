95cc38dd4368ed7d9303e171eb441a640803b367
Ritek Rounak <ritek.rounak@harness.io>
2024-10-29 07:17:08 +0530

    feat: [CDS-101927]: Fix Service_override_v2

--- internal/service/cd_nextgen/service_overrides_v2/resource_service_overrides_v2_test.go
+++ internal/service/cd_nextgen/service_overrides_v2/resource_service_overrides_v2_test.go
@@ -267,12 +267,60 @@ func testRemoteServiceOverrides(id string, name string) string {
     org_id = harness_platform_organization.test.id
     color = "#472848"
   }
+resource "harness_platform_environment" "test" {
+			identifier = "%[1]s"
+			name = "%[2]s"
+			org_id = harness_platform_organization.test.id
+			project_id = harness_platform_project.test.id
+			tags = ["foo:bar", "baz"]
+			type = "PreProduction"
+  	}
+
+		resource "harness_platform_service" "test" {
+			identifier = "%[1]s"
+			name = "%[2]s"
+			org_id = harness_platform_organization.test.id
+			project_id = harness_platform_project.test.id
+			yaml = <<-EOT
+        service:
+          name: %[1]s
+          identifier: %[2]s
+          serviceDefinition:
+            spec:
+              manifests:
+                - manifest:
+                    identifier: manifest1
+                    type: Values
+                    spec:
+                      store:
+                        type: Github
+                        spec:
+                          connectorRef: <+input>
+                          gitFetchType: Branch
+                          paths:
+                            - files1
+                          repoName: <+input>
+                          branch: master
+                      skipResourceVersioning: false
+              configFiles:
+                - configFile:
+                    identifier: configFile1
+                    spec:
+                      store:
+                        type: Harness
+                        spec:
+                          files:
+                            - <+org.description>
+            type: Kubernetes
+          gitOpsEnabled: false
+		  EOT
+		}
 
   resource "harness_platform_service_overrides_v2" "test" {
     org_id     = harness_platform_organization.test.id
     project_id = harness_platform_project.test.id
-     env_id     = "account.TF_GitX_connector"
-     service_id = "account.TF_GitX_connector"
+     env_id     = harness_platform_environment.test.id
+     service_id = harness_platform_service.test.id
      type       = "ENV_SERVICE_OVERRIDE"
      git_details {
        store_type = "REMOTE"
@@ -294,11 +342,11 @@ func testRemoteServiceOverrides(id string, name string) string {
                store:
                  type: Github
                  spec:
-                   connectorRef: "<+input>"
+                   connectorRef: <+input>
                    gitFetchType: Branch
                    paths:
                      - files1
-                   repoName: "<+input>"
+                   repoName: <+input>
                    branch: master
                skipResourceVersioning: false
        configFiles:
@@ -309,7 +357,7 @@ func testRemoteServiceOverrides(id string, name string) string {
                  type: Harness
                  spec:
                    files:
-                     - "<+org.description>"
+                     - <+org.description>
                      EOT
 }`, id, name)
 }
