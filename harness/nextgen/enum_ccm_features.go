f4e5cad6acef4e98aadee28983feb5fda6ecd48e
Anmol Maheshwari <anmol.maheshwari@harness.io>
2024-11-06 14:58:56 +0530

    feat:[CCM-20085]: Add Governance as feature in CCM Connector Features (#595)

--- harness/nextgen/enum_ccm_features.go
+++ harness/nextgen/enum_ccm_features.go
@@ -6,16 +6,19 @@ var CCMFeatures = struct {
 	Billing      CCMFeature
 	Optimization CCMFeature
 	Visibility   CCMFeature
+	Governance   CCMFeature
 }{
 	Billing:      "BILLING",
 	Optimization: "OPTIMIZATION",
 	Visibility:   "VISIBILITY",
+	Governance:   "GOVERNANCE",
 }
 
 var CCMFeaturesSlice = []string{
 	CCMFeatures.Billing.String(),
 	CCMFeatures.Optimization.String(),
 	CCMFeatures.Visibility.String(),
+	CCMFeatures.Governance.String(),
 }
 
 func (e CCMFeature) String() string {
