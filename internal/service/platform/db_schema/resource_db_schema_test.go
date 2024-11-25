21a12f52c8dff5f0077c3cdbdd9016f3234edd5c
Hemanth Sridhar <hemanth.sridhar@harness.io>
2024-11-25 20:25:10 +0530

    fix: [DBOPS-743]: Added archivePath support

--- internal/service/platform/db_schema/resource_db_schema_test.go
+++ internal/service/platform/db_schema/resource_db_schema_test.go
@@ -47,7 +47,48 @@ func TestAccResourceDBSchema(t *testing.T) {
 				ImportState:             true,
 				ImportStateVerify:       true,
 				ImportStateIdFunc:       acctest.ProjectResourceImportStateIdFunc(resourceName),
-				ImportStateVerifyIgnore: []string{"schema_source.#", "schema_source.0.%", "schema_source.0.connector", "schema_source.0.location", "schema_source.0.repo"},
+				ImportStateVerifyIgnore: []string{"schema_source.#", "schema_source.0.%", "schema_source.0.connector", "schema_source.0.location", "schema_source.0.repo", "schema_source.0.archive_path"},
+			},
+		},
+	})
+}
+
+func TestAccResourceDBSchemaArtifactory(t *testing.T) {
+
+	name := t.Name()
+	id := fmt.Sprintf("%s_%s", name, utils.RandStringBytes(5))
+	updatedName := fmt.Sprintf("%s_updated", name)
+	resourceName := "harness_platform_db_schema.test"
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		CheckDestroy:      testAccDBSchemaDestroy(resourceName),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceDBSchemaArtifactory(id, name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", id),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "org_id", id),
+					resource.TestCheckResourceAttr(resourceName, "project_id", id),
+				),
+			},
+			{
+				Config: testAccResourceDBSchemaArtifactory(id, updatedName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", id),
+					resource.TestCheckResourceAttr(resourceName, "name", updatedName),
+					resource.TestCheckResourceAttr(resourceName, "org_id", id),
+					resource.TestCheckResourceAttr(resourceName, "project_id", id),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateIdFunc:       acctest.ProjectResourceImportStateIdFunc(resourceName),
+				ImportStateVerifyIgnore: []string{"schema_source.#", "schema_source.0.%", "schema_source.0.connector", "schema_source.0.location", "schema_source.0.repo", "schema_source.0.archive_path"},
 			},
 		},
 	})
@@ -122,6 +163,45 @@ func testAccResourceDBSchema(id string, name string) string {
 							connector = "%[1]s"
 							repo = "TestRepo"
 							location = "db/example-changelog.yaml"
+                        }
+ 
+        }
+        `, id, name)
+}
+
+func testAccResourceDBSchemaArtifactory(id string, name string) string {
+	return fmt.Sprintf(`
+				resource "harness_platform_organization" "test" {
+					identifier = "%[1]s"
+					name = "%[2]s"
+				}
+				resource "harness_platform_project" "test" {
+					identifier = "%[1]s"
+					name = "%[2]s"
+					org_id = harness_platform_organization.test.id
+					color = "#472848"
+				}
+				resource "harness_platform_connector_artifactory" "test" {
+					identifier  = "%[1]s"
+					name        = "%[2]s"
+					org_id = harness_platform_project.test.org_id
+					project_id = harness_platform_project.test.id
+					description = "test"
+  					tags        = ["foo:bar"]
+  					url                = "https://artifactory.example.com"
+  					delegate_selectors = ["harness-delegate"]
+				}
+        resource "harness_platform_db_schema" "test" {
+                        identifier = "%[1]s"
+                        org_id = harness_platform_project.test.org_id
+						project_id = harness_platform_project.test.id
+                        name = "%[2]s"
+                        tags = ["foo:bar", "bar:foo"]
+                        schema_source {
+							connector = "%[1]s"
+							repo = "TestRepo"
+							location = "db/example-changelog.yaml"
+							archive_path = "bla.zip"
                         }
  
         }
