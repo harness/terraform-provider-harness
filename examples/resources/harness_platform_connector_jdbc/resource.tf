465bef35fcd2def7a989d925704e45af5c09d09e
Abhishek <abhishek.abhishek@harness.io>
2025-01-26 12:49:42 +0530

    [feat]: [DBOPS-845]: JDBC connector ServiceAccount auth

--- examples/resources/harness_platform_connector_jdbc/resource.tf
+++ examples/resources/harness_platform_connector_jdbc/resource.tf
@@ -6,7 +6,69 @@ resource "harness_platform_connector_jdbc" "test" {
   url                = "jdbc:sqlserver://1.2.3;trustServerCertificate=true"
   delegate_selectors = ["harness-delegate"]
   credentials {
+    auth_type = "ServiceAccount"
+    service_account {
+      token_ref = "account.secret_id"
+    }
+  }
+}
+
+resource "harness_platform_connector_jdbc" "test" {
+  identifier         = "identifer"
+  name               = "name"
+  description        = "test"
+  tags               = ["foo:bar"]
+  url                = "jdbc:sqlserver://1.2.3;trustServerCertificate=true"
+  delegate_selectors = ["harness-delegate"]
+  credentials {
+    auth_type = "UsernamePassword"
+    username_password {
+      username     = "admin"
+      password_ref = "account.secret_id"
+    }
+  }
+}
+
+resource "harness_platform_connector_jdbc" "test" {
+  identifier         = "identifer"
+  name               = "name"
+  description        = "test"
+  tags               = ["foo:bar"]
+  url                = "jdbc:sqlserver://1.2.3;trustServerCertificate=true"
+  delegate_selectors = ["harness-delegate"]
+  credentials {
+    auth_type = "UsernamePassword"
+    username_password {
+      username_ref     = "account.user_ref"
+      password_ref = "account.secret_id"
+    }
+  }
+}
+
+resource "harness_platform_connector_jdbc" "test" {
+  identifier         = "identifer"
+  name               = "name"
+  description        = "test"
+  tags               = ["foo:bar"]
+  url                = "jdbc:sqlserver://1.2.3;trustServerCertificate=true"
+  delegate_selectors = ["harness-delegate"]
+  credentials {
+    auth_type = "UsernamePassword"
     username     = "admin"
+    password_ref = "account.secret_id"
+  }
+}
+
+resource "harness_platform_connector_jdbc" "test" {
+  identifier         = "identifer"
+  name               = "name"
+  description        = "test"
+  tags               = ["foo:bar"]
+  url                = "jdbc:sqlserver://1.2.3;trustServerCertificate=true"
+  delegate_selectors = ["harness-delegate"]
+  credentials {
+    auth_type = "UsernamePassword"
+    username_ref     = "account.user_ref"
     password_ref = "account.secret_id"
   }
 }
--- internal/service/platform/connector/jdbc_data_source.go
+++ internal/service/platform/connector/jdbc_data_source.go
@@ -28,6 +28,11 @@ func DatasourceConnectorJDBC() *schema.Resource {
 				Computed:    true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
+						"auth_type": {
+							Description: "Authentication types for JDBC connector",
+							Type:        schema.TypeString,
+							Computed:    true,
+						},
 						"username": {
 							Description: "The username to use for the database server.",
 							Type:        schema.TypeString,
@@ -42,6 +47,44 @@ func DatasourceConnectorJDBC() *schema.Resource {
 							Description: "The reference to the Harness secret containing the password to use for the database server." + secret_ref_text,
 							Type:        schema.TypeString,
 							Computed:    true,
+						},
+						"username_password": {
+							Description: "Authenticate using username password.",
+							Type:        schema.TypeList,
+							Computed:    true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"username": {
+										Description: "Username to use for authentication.",
+										Type:        schema.TypeString,
+										Computed:    true,
+									},
+									"username_ref": {
+										Description: "Reference to a secret containing the username to use for authentication." + secret_ref_text,
+										Type:        schema.TypeString,
+										Computed:    true,
+									},
+									"password_ref": {
+										Description: "Reference to a secret containing the password to use for authentication." + secret_ref_text,
+										Type:        schema.TypeString,
+										Computed:    true,
+									},
+								},
+							},
+						},
+						"service_account": {
+							Description: "Authenticate using service account.",
+							Type:        schema.TypeList,
+							Computed:    true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"token_ref": {
+										Description: "Reference to a secret containing the token to use for authentication." + secret_ref_text,
+										Type:        schema.TypeString,
+										Computed:    true,
+									},
+								},
+							},
 						},
 					},
 				},
