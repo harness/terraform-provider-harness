a0f5dc91adf6a4653ab43d3da98d474cfcd05be5
Imran Khan <imran.khan@harness.io>
2025-02-15 16:58:12 +0530

    reverted resource.tf

--- examples/resources/harness_platform_resource_group/resource.tf
+++ examples/resources/harness_platform_resource_group/resource.tf
@@ -1,35 +1,23 @@
-terraform {
-  required_providers {
-    harness = {
-      source = "harness/harness"
-    }
-  }
-}
-
 resource "harness_platform_resource_group" "test" {
-  identifier  = "fromTwF"
-  name        = "fromTwF"
+  identifier  = "identifier"
+  name        = "name"
   description = "test"
   tags        = ["foo:bar"]
 
-  org_id =  "default"
-  project_id = "ResourceGroupTest"
-  account_id = "Cdrf1gJ7RQOqMEGWV_5FTA"
-  allowed_scope_levels = ["project"]
+  account_id           = "account_id"
+  allowed_scope_levels = ["account"]
   included_scopes {
     filter     = "EXCLUDING_CHILD_SCOPES"
-    account_id = "Cdrf1gJ7RQOqMEGWV_5FTA"
-    org_id =  "default"
-    project_id = "ResourceGroupTest"
+    account_id = "account_id"
   }
   resource_filter {
     include_all_resources = false
     resources {
-      resource_type = "ENVIRONMENT"
-      identifiers = [
-          "invalid",
-          "ok-bye"
-      ]
+      resource_type = "CONNECTOR"
+      attribute_filter {
+        attribute_name   = "category"
+        attribute_values = ["CLOUD_COST"]
+      }
     }
   }
 }
