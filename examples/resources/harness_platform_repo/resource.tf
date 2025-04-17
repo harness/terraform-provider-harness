04795b4d368f0bf83fd2e7277186f8c2a148c3e0
Abhinav Singh <abhinav.singh@harness.io>
2025-04-17 15:14:09 -0700

    fix: change doc for harness code to not have name (#1233)

--- examples/resources/harness_platform_repo/resource.tf
+++ examples/resources/harness_platform_repo/resource.tf
@@ -1,6 +1,5 @@
 resource "harness_platform_repo" "test" {
   identifier     = "test_repo_123"
-  name           = "test_repo_123"
   org_id         = "test_org_123"
   project_id     = "test_project_123"
   default_branch = "main"
