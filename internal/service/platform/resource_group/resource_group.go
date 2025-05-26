e1a9cde731ef6f1a868994da8e00730060ba7202
Imran Khan <imran.khan@harness.io>
2025-05-26 12:11:46 +0530

    imported log

--- internal/service/platform/resource_group/resource_group.go
+++ internal/service/platform/resource_group/resource_group.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"log"
 )
 
 func ResourceResourceGroup() *schema.Resource {
