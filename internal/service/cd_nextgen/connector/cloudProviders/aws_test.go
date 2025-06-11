8c771b77a059b298796946fbdb1165204e340076
Harness Admin <jeremy.nelson@harness.io>
2025-06-11 16:56:58 +0530

    fix: [CDS-110782]: Adding UT for execute_on_delegate Validation

--- internal/service/cd_nextgen/connector/cloudProviders/aws_test.go
+++ internal/service/cd_nextgen/connector/cloudProviders/aws_test.go
@@ -127,6 +127,7 @@ func TestAccResourceConnectorAws_Irsa(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "irsa.0.delegate_selectors.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "irsa.0.region", "us-east-1"),
+					resource.TestCheckResourceAttr(resourceName, "execute_on_delegate", "true"),
 				),
 			},
 			{
@@ -162,6 +163,42 @@ func TestAccResourceConnectorAws_Manual(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "manual.0.delegate_selectors.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "manual.0.region", "us-east-1"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccResourceConnectorAws_executeOnDelegateTrue(t *testing.T) {
+
+	id := fmt.Sprintf("%s_%s", t.Name(), utils.RandStringBytes(5))
+	name := id
+	resourceName := "harness_platform_connector_aws.test"
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"time": {},
+		},
+		CheckDestroy: testAccConnectorDestroy(resourceName),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceConnectorAws_manual(id, name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "id", id),
+					resource.TestCheckResourceAttr(resourceName, "identifier", id),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "description", "test"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "manual.0.delegate_selectors.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "manual.0.region", "us-east-1"),
+					resource.TestCheckResourceAttr(resourceName, "execute_on_delegate", "true"),
 				),
 			},
 			{
@@ -377,7 +414,7 @@ func testAccResourceConnectorAws_irsa(id string, name string) string {
 			name = "%[2]s"
 			description = "test"
 			tags = ["foo:bar"]
-
+            execute_on_delegate = true
 			irsa {
 				delegate_selectors = ["harness-delegate"]
 				region = "us-east-1"
@@ -404,7 +441,7 @@ func testAccResourceConnectorAws_manual(id string, name string) string {
 			name = "%[2]s"
 			description = "test"
 			tags = ["foo:bar"]
-
+            execute_on_delegate = true
 			manual {
 				access_key_ref = "account.${harness_platform_secret_text.test.id}"
 				secret_key_ref = "account.${harness_platform_secret_text.test.id}"
