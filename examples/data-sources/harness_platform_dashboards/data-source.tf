641c08d7628c7b3cf8a4847012fc97ed81c86f20
Jonathan Khattak <jonathan.khattak@harness.io>
2025-06-17 11:49:22 +0100

    feat: [PL-48875]: Updated data-source.tf. removed import.sh

--- examples/data-sources/harness_platform_dashboards/data-source.tf
+++ examples/data-sources/harness_platform_dashboards/data-source.tf
@@ -1,6 +1,3 @@
 data "harness_platform_dashboards" "dashboard" {
-  dashboard_id          = "dashboard_id"
-  description           = "description"
-  resource_identifier   = "resource_identifier"
-  title                 = "title"
+  id  = "id"
 }
