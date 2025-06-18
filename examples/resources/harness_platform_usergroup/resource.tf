a3039656e854e3b0589e0a0084e5cad49317af54
Abhishek Das <abhishek.das@harness.io>
2025-06-18 16:00:34 +0530

    Updated resource.tf

--- examples/resources/harness_platform_usergroup/resource.tf
+++ examples/resources/harness_platform_usergroup/resource.tf
@@ -6,15 +6,6 @@ terraform {
   }
 }
 
-resource "harness_platform_usergroup" "abhiTerraformUGV1" {
-  identifier         = "abhiTerraformUGV1"
-  name               = "abhiTerraformUGV1"
-  # org_id             = "abhishek_test"
-  # project_id         = "abhishek_test"
-  externally_managed = false
-  user_emails        = ["abhishek.das+3@harness.io"]
-}
-
 resource "harness_platform_usergroup" "abhiTerraformUGV3" {
   identifier         = "abhiTerraformUGV3"
   name               = "abhiTerraformUGV3"
