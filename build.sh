85b288f8491141ba89bfcbad49709e30c7f59621
Gokul Bansal <gokul.bansal@harness.io>
2025-07-10 15:10:03 +0530

    feat: [PL-63595]: add revoke after field in delegate token provider

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-version=0.37.13
-source=registry.terraform.io/harness/harness
-platform=darwin_arm64
-mkdir -p ~/.terraform.d/plugins/$source/$version/$platform/
-echo "HI"
-cp terraform-provider-harness ~/.terraform.d/plugins/$source/$version/$platform/terraform-provider-harness
-echo "Done"
