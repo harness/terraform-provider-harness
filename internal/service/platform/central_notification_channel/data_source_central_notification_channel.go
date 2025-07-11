f753e9338812bc19de519ddcb81aefe259b56ec2
akshatgoyal <akshat.goyal@harness.io>
2025-07-11 10:19:17 +0530

    codeCleanup

--- internal/service/platform/central_notification_channel/data_source_central_notification_channel.go
+++ internal/service/platform/central_notification_channel/data_source_central_notification_channel.go
@@ -1,21 +1,14 @@
 package central_notification_channel
 
 import (
-	"context"
-	"github.com/antihax/optional"
-	"github.com/harness/harness-go-sdk/harness/nextgen"
-	"github.com/harness/terraform-provider-harness/helpers"
-	"github.com/harness/terraform-provider-harness/internal"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"net/http"
 )
 
 func DataSourceCentralNotificationChannelService() *schema.Resource {
 	return &schema.Resource{
 		Description: "Data source for retrieving a central notification channel in Harness.",
 
-		ReadContext: dataCentralNotificationChannelRead,
+		ReadContext: resourceCentralNotificationChannelRead,
 
 		Schema: map[string]*schema.Schema{
 			"identifier": {
@@ -160,44 +153,4 @@ func DataSourceCentralNotificationChannelService() *schema.Resource {
 			},
 		},
 	}
-}
-
-func dataCentralNotificationChannelRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	c, ctx := meta.(*internal.Session).GetPlatformClientWithContext(ctx)
-	var accountIdentifier, orgIdentifier, projectIdentifier, identifier string
-	accountIdentifier = c.AccountId
-	if attr, ok := d.GetOk("org"); ok {
-		orgIdentifier = attr.(string)
-	}
-	if attr, ok := d.GetOk("project"); ok {
-		projectIdentifier = attr.(string)
-	}
-
-	identifier = d.Get("identifier").(string)
-
-	var resp nextgen.NotificationChannelDto
-	var httpResp *http.Response
-	var err error
-	if orgIdentifier != "" && projectIdentifier != "" {
-		resp, httpResp, err = c.NotificationChannelsApi.GetNotificationChannel(ctx, identifier, orgIdentifier, projectIdentifier,
-			&nextgen.NotificationChannelsApiGetNotificationChannelOpts{
-				HarnessAccount: optional.NewString(accountIdentifier),
-			})
-	} else if orgIdentifier != "" {
-		resp, httpResp, err = c.NotificationChannelsApi.GetNotificationChannelOrg(ctx, identifier, orgIdentifier,
-			&nextgen.NotificationChannelsApiGetNotificationChannelOrgOpts{
-				HarnessAccount: optional.NewString(accountIdentifier),
-			})
-	} else {
-		resp, httpResp, err = c.NotificationChannelsApi.GetNotificationChannelAccount(ctx, identifier,
-			&nextgen.NotificationChannelsApiGetNotificationChannelAccountOpts{
-				HarnessAccount: optional.NewString(accountIdentifier),
-			})
-	}
-
-	if err != nil {
-		return helpers.HandleReadApiError(err, d, httpResp)
-	}
-	readCentralNotificationChannel(accountIdentifier, d, resp)
-	return nil
 }
