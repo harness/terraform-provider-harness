02849615752346be13b38e56b45480d84de7e7b8
Sagar Kumar <sagar.kumar@harness.io>
2025-07-21 00:25:02 -0700

    feat: [CHAOS-8828]: Update image registry docs
    Signed-off-by: Sagar Kumar <sagar.kumar@harness.io>

--- examples/resources/harness_chaos_image_registry/import.sh
+++
@@ -1,8 +0,0 @@
-# Import Project level Chaos Image Registry
-terraform import harness_chaos_image_registry.example <org_id>/<project_id>/<environment_id>/<infra_id>
-
-# Import Org level Chaos Image Registry
-terraform import harness_chaos_image_registry.example <org_id>/<environment_id>/<infra_id>
-
-# Import Account level Chaos Image Registry
-terraform import harness_chaos_image_registry.example <environment_id>/<infra_id>
