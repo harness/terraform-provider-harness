3ab6e881fad12c7fca74a20805b17362a1a1ac16
Aayush Negi <aayush.negi@harness.io>
2025-08-21 13:20:35 +0530

    Added UpdateContext to Delegatetoken provider which calls REVOKE API

--- internal/service/platform/delegate_token/resource_delegateToken_test.go
+++ internal/service/platform/delegate_token/resource_delegateToken_test.go
@@ -105,6 +105,95 @@ func TestAccResourceDelegateTokenWithRevokeAfter(t *testing.T) {
 	})
 }
 
+//will add these tests when change DELETE CONTEXT to call delete delegate token api
+
+// func TestAccResourceDelegateTokenUpdate(t *testing.T) {
+// 	name := utils.RandStringBytes(5)
+// 	account_id := os.Getenv("HARNESS_ACCOUNT_ID")
+
+// 	resourceName := "harness_platform_delegatetoken.test"
+
+// 	resource.UnitTest(t, resource.TestCase{
+// 		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+// 		ProviderFactories: acctest.ProviderFactories,
+// 		Steps: []resource.TestStep{
+// 			{
+// 				Config: tesAccResourceDelegateToken(name, account_id),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr(resourceName, "name", name),
+// 					resource.TestCheckResourceAttr(resourceName, "token_status", "ACTIVE"),
+// 				),
+// 			},
+// 			{
+// 				Config: tesAccResourceDelegateTokenUpdateAccountLevelUpdated(name, account_id),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr(resourceName, "name", name),
+// 					resource.TestCheckResourceAttr(resourceName, "token_status", "REVOKED"),
+// 				),
+// 			},
+// 		},
+// 	})
+// }
+
+// func TestAccResourceDelegateTokenUpdateOrgLevel(t *testing.T) {
+// 	name := utils.RandStringBytes(5)
+// 	account_id := os.Getenv("HARNESS_ACCOUNT_ID")
+// 	org_id := utils.RandStringBytes(5)
+
+// 	resourceName := "harness_platform_delegatetoken.test"
+
+// 	resource.UnitTest(t, resource.TestCase{
+// 		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+// 		ProviderFactories: acctest.ProviderFactories,
+// 		Steps: []resource.TestStep{
+// 			{
+// 				Config: tesAccResourceDelegateTokenOrgLevel(name, account_id, org_id),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr(resourceName, "name", name),
+// 					resource.TestCheckResourceAttr(resourceName, "token_status", "ACTIVE"),
+// 				),
+// 			},
+// 			{
+// 				Config: tesAccResourceDelegateTokenUpdateOrgLevelUpdated(name, account_id, org_id),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr(resourceName, "name", name),
+// 					resource.TestCheckResourceAttr(resourceName, "token_status", "REVOKED"),
+// 				),
+// 			},
+// 		},
+// 	})
+// }
+
+// func TestAccResourceDelegateTokenUpdateProjectLevel(t *testing.T) {
+// 	name := utils.RandStringBytes(5)
+// 	account_id := os.Getenv("HARNESS_ACCOUNT_ID")
+// 	org_id := utils.RandStringBytes(5)
+// 	project_id := utils.RandStringBytes(5)
+
+// 	resourceName := "harness_platform_delegatetoken.test"
+
+// 	resource.UnitTest(t, resource.TestCase{
+// 		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+// 		ProviderFactories: acctest.ProviderFactories,
+// 		Steps: []resource.TestStep{
+// 			{
+// 				Config: tesAccResourceDelegateTokenProjectLevel(name, account_id, org_id, project_id),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr(resourceName, "name", name),
+// 					resource.TestCheckResourceAttr(resourceName, "token_status", "ACTIVE"),
+// 				),
+// 			},
+// 			{
+// 				Config: tesAccResourceDelegateTokenUpdateProjectLevelUpdated(name, account_id, org_id, project_id),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr(resourceName, "name", name),
+// 					resource.TestCheckResourceAttr(resourceName, "token_status", "REVOKED"),
+// 				),
+// 			},
+// 		},
+// 	})
+// }
+
 func tesAccResourceDelegateToken(name string, accountId string) string {
 	return fmt.Sprintf(`
 		resource "harness_platform_delegatetoken" "test" {			
@@ -151,6 +240,58 @@ func tesAccResourceDelegateTokenProjectLevel(name string, accountId string, org_
 		}
 		`, name, accountId, org_id, project_id)
 }
+
+//will add these tests when change DELETE CONTEXT to call delete delegate token api
+
+// func tesAccResourceDelegateTokenUpdateAccountLevelUpdated(name string, accountId string) string {
+// 	return fmt.Sprintf(`
+// 		resource "harness_platform_delegatetoken" "test" {
+// 			name = "%[1]s"
+// 			account_id = "%[2]s"
+// 			token_status = "REVOKED"
+// 		}
+// 		`, name, accountId)
+// }
+
+// func tesAccResourceDelegateTokenUpdateOrgLevelUpdated(name string, accountId string, org_id string) string {
+// 	return fmt.Sprintf(`
+// 		resource "harness_platform_organization" "test" {
+// 			identifier = "%[1]s"
+// 			name = "%[1]s"
+// 		}
+
+// 		resource "harness_platform_delegatetoken" "test" {
+// 			name = "%[1]s"
+// 			account_id = "%[2]s"
+// 			org_id = harness_platform_organization.test.id
+// 			token_status = "REVOKED"
+// 		}
+// 		`, name, accountId, org_id)
+// }
+
+// func tesAccResourceDelegateTokenUpdateProjectLevelUpdated(name string, accountId string, org_id string, project_id string) string {
+// 	return fmt.Sprintf(`
+// 		resource "harness_platform_organization" "test" {
+// 			identifier = "%[3]s"
+// 			name = "%[1]s"
+// 		}
+
+// 		resource "harness_platform_project" "test" {
+// 			identifier = "%[4]s"
+// 			name = "%[1]s"
+// 			org_id = harness_platform_organization.test.id
+// 			color = "#472848"
+// 		}
+
+// 		resource "harness_platform_delegatetoken" "test" {
+// 			name = "%[1]s"
+// 			account_id = "%[2]s"
+// 			org_id = harness_platform_organization.test.id
+// 			project_id = harness_platform_project.test.id
+// 			token_status = "REVOKED"
+// 		}
+// 		`, name, accountId, org_id, project_id)
+// }
 
 func testAccResourceDelegateTokenWithRevokeAfter(name string, accountId string) string {
 	return fmt.Sprintf(`
