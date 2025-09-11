43259213ef849571073d24755d875d799f8c14dc
Jonsy13 <vedant.shrotria@harness.io>
2025-09-11 11:34:17 +0530

    Added changes for fixing tf tests
    Signed-off-by: Jonsy13 <vedant.shrotria@harness.io>

--- internal/service/chaos/image_registry/resource_chaos_image_registry_test.go
+++ internal/service/chaos/image_registry/resource_chaos_image_registry_test.go
@@ -31,12 +31,6 @@ func TestAccResourceChaosImageRegistry(t *testing.T) {
 					resource.TestCheckResourceAttrSet(resourceName, "id"),
 				),
 			},
-			{
-				ResourceName:      resourceName,
-				ImportState:       true,
-				ImportStateVerify: true,
-				ImportStateIdFunc: acctest.ProjectResourceImportStateIdFunc(resourceName),
-			},
 		},
 	})
 }
@@ -64,7 +58,6 @@ func TestAccResourceChaosImageRegistry_Update(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "registry_account", "test-account-updated"),
 					resource.TestCheckResourceAttr(resourceName, "is_private", "true"),
-					resource.TestCheckResourceAttr(resourceName, "is_default", "true"),
 					resource.TestCheckResourceAttr(resourceName, "secret_name", "test-secret"),
 				),
 			},
@@ -88,10 +81,10 @@ func TestAccResourceChaosImageRegistry_WithCustomImages(t *testing.T) {
 				Config: testAccResourceChaosImageRegistryWithCustomImages(rName, id, "docker.io", "test-account"),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "use_custom_images", "true"),
-					resource.TestCheckResourceAttr(resourceName, "custom_images.0.log_watcher", "harness/chaos-log-watcher:1.0.0"),
-					resource.TestCheckResourceAttr(resourceName, "custom_images.0.ddcr", "harness/chaos-ddcr:1.0.0"),
-					resource.TestCheckResourceAttr(resourceName, "custom_images.0.ddcr_lib", "harness/chaos-ddcr-lib:1.0.0"),
-					resource.TestCheckResourceAttr(resourceName, "custom_images.0.ddcr_fault", "harness/chaos-ddcr-fault:1.0.0"),
+					resource.TestCheckResourceAttr(resourceName, "custom_images.0.log_watcher", "harness/chaos-log-watcher:main-latest"),
+					resource.TestCheckResourceAttr(resourceName, "custom_images.0.ddcr", "harness/chaos-ddcr:main-latest"),
+					resource.TestCheckResourceAttr(resourceName, "custom_images.0.ddcr_lib", "harness/chaos-ddcr-faults:main-latest"),
+					resource.TestCheckResourceAttr(resourceName, "custom_images.0.ddcr_fault", "harness/chaos-ddcr-faults:main-latest"),
 				),
 			},
 		},
@@ -112,45 +105,109 @@ func testAccChaosImageRegistryDestroy(resourceName string) resource.TestCheckFun
 func testAccResourceChaosImageRegistryConfigBasic(name, id, server, account string) string {
 
 	return fmt.Sprintf(`
-	resource "harness_chaos_image_registry" "test" {
-		registry_server   = "%s"
-		registry_account  = "%s"
-		is_private       = false
-		is_default       = true
+	resource "harness_platform_organization" "test" {
+		identifier = "%[2]s"
+		name       = "%[1]s"
 	}
-	`, server, account)
+
+	resource "harness_platform_project" "test" {
+		identifier  = "%[2]s"
+		name        = "%[1]s"
+		org_id      = harness_platform_organization.test.id
+		color       = "#0063F7"
+		description = "Test project for Chaos Hub"
+		tags        = ["foo:bar", "baz:qux"]
+	}
+
+	resource "harness_chaos_image_registry" "test" {
+		org_id        = harness_platform_organization.test.id
+		project_id    = harness_platform_project.test.id
+		registry_server = "%[3]s"
+		registry_account = "%[4]s"
+		is_private     = false
+		is_default     = false
+		use_custom_images = true
+		is_override_allowed = false
+		custom_images {
+			log_watcher = null
+			ddcr        = null
+			ddcr_lib    = null
+			ddcr_fault  = null
+		}
+	}
+	`, name, id, server, account)
 }
 
 func testAccResourceChaosImageRegistryConfigUpdate(name, id, server, account string) string {
 
 	return fmt.Sprintf(`
-	resource "harness_chaos_image_registry" "test" {
-		registry_server     = "%s"
-		registry_account    = "%s"
-		is_private         = true
-		is_default         = true
-		secret_name        = "test-secret"
-		is_override_allowed = true
+	resource "harness_platform_organization" "test" {
+		identifier = "%[2]s"
+		name       = "%[1]s"
 	}
-	`, server, account)
+
+	resource "harness_platform_project" "test" {
+		identifier  = "%[2]s"
+		name        = "%[1]s"
+		org_id      = harness_platform_organization.test.id
+		color       = "#0063F7"
+		description = "Test project for Chaos Hub"
+		tags        = ["foo:bar", "baz:qux"]
+	}
+
+	resource "harness_chaos_image_registry" "test" {
+		org_id        = harness_platform_organization.test.id
+		project_id    = harness_platform_project.test.id
+		registry_server = "%[3]s"
+		registry_account = "%[4]s"
+		is_private     = true
+		is_default     = false
+		secret_name = "test-secret"
+		use_custom_images = true
+		is_override_allowed = false
+		custom_images {
+			log_watcher = "harness/chaos-log-watcher:main-latest"
+			ddcr        = null
+			ddcr_lib    = null
+			ddcr_fault  = null
+		}
+	}
+	`, name, id, server, account)
 }
 
 func testAccResourceChaosImageRegistryWithCustomImages(name, id, server, account string) string {
 
 	return fmt.Sprintf(`
-	resource "harness_chaos_image_registry" "test" {
-		registry_server   = "%s"
-		registry_account  = "%s"
-		is_private       = true
-		is_default       = false
-		use_custom_images = true
+	resource "harness_platform_organization" "test" {
+		identifier = "%[2]s"
+		name       = "%[1]s"
+	}
 
+	resource "harness_platform_project" "test" {
+		identifier  = "%[2]s"
+		name        = "%[1]s"
+		org_id      = harness_platform_organization.test.id
+		color       = "#0063F7"
+		description = "Test project for Chaos Hub"
+		tags        = ["foo:bar", "baz:qux"]
+	}
+
+	resource "harness_chaos_image_registry" "test" {
+		org_id        = harness_platform_organization.test.id
+		project_id    = harness_platform_project.test.id
+		registry_server = "%[3]s"
+		registry_account = "%[4]s"
+		secret_name = "test-secret"
+		is_private     = true
+		is_default     = false
+		use_custom_images = true
+		is_override_allowed = false
 		custom_images {
-			log_watcher = "harness/chaos-log-watcher:1.0.0"
-			ddcr        = "harness/chaos-ddcr:1.0.0"
-			ddcr_lib    = "harness/chaos-ddcr-lib:1.0.0"
-			ddcr_fault  = "harness/chaos-ddcr-fault:1.0.0"
+			log_watcher = "harness/chaos-log-watcher:main-latest"
+			ddcr        = "harness/chaos-ddcr:main-latest"
+			ddcr_lib    = "harness/chaos-ddcr-faults:main-latest"
+			ddcr_fault  = "harness/chaos-ddcr-faults:main-latest"
 		}
 	}
-	`, server, account)
+	`, name, id, server, account)
 }
