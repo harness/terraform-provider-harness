4499a2e4f8f2d709be214dfc3a67aa71b28dc773
Shivam  Thakur <shivam.thakur@harness.io>
2025-10-08 05:15:00 +0000

    feat: [CCM-26311]: Support disabled field through terraform (#1301)
    * ebec10 documentation updated
    
    * 0b1fa9 sdk version update
    
    * dd50fc add disabled in example
    
    * 815f4d add disabled in data source
    
    * 78aaf1 tests
    
    * ad77b9 add disabled field support in resource

--- internal/service/platform/cluster_orchestrator/clusterorch_config_test.go
+++ internal/service/platform/cluster_orchestrator/clusterorch_config_test.go
@@ -19,7 +19,41 @@ func TestResourceClusterOrchestratorConfig(t *testing.T) {
 			{
 				Config: testClusterOrchConfig(orchID),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "orchID", orchID),
+					resource.TestCheckResourceAttr(resourceName, "orchestrator_id", orchID),
+					resource.TestCheckResourceAttr(resourceName, "disabled", "false"),
+				),
+			},
+		},
+	})
+}
+
+func TestResourceClusterOrchestratorConfigDisabled(t *testing.T) {
+	orchID := "terraform-clusterorch-disabled-test"
+	resourceName := "harness_cluster_orchestrator_config.test"
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.TestAccPreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testClusterOrchConfigDisabled(orchID, false),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "orchestrator_id", orchID),
+					resource.TestCheckResourceAttr(resourceName, "disabled", "false"),
+				),
+			},
+			{
+				Config: testClusterOrchConfigDisabled(orchID, true),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "orchestrator_id", orchID),
+					resource.TestCheckResourceAttr(resourceName, "disabled", "true"),
+				),
+			},
+			{
+				Config: testClusterOrchConfigDisabled(orchID, false),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "orchestrator_id", orchID),
+					resource.TestCheckResourceAttr(resourceName, "disabled", "false"),
 				),
 			},
 		},
@@ -30,6 +64,7 @@ func testClusterOrchConfig(orchID string) string {
 	return fmt.Sprintf(`
 	resource "harness_cluster_orchestrator_config" "test" {
 		orchestrator_id = "%s"
+		disabled        = false
 		distribution {
 			base_ondemand_capacity = 1
             ondemand_replica_percentage = 60
@@ -89,4 +124,70 @@ func testClusterOrchConfig(orchID string) string {
 		}
 	}
 `, orchID)
+}
+
+func testClusterOrchConfigDisabled(orchID string, disabled bool) string {
+	return fmt.Sprintf(`
+	resource "harness_cluster_orchestrator_config" "test" {
+		orchestrator_id = "%s"
+		disabled        = %t
+		distribution {
+			base_ondemand_capacity = 1
+            ondemand_replica_percentage = 60
+            selector = "ALL"
+            strategy = "COST OPTIMIZED"
+		}
+		binpacking {
+            pod_eviction {
+				threshold {
+					cpu = 60
+					memory = 80
+				}
+			}
+			disruption {
+				criteria = "EmptyOrUnderUtilized"
+                delay = "10m"
+                budget {
+                	reasons = ["Drift","UnderUtilized","Empty"]
+                    nodes = "20"
+					schedule {
+						frequency = "@daily"
+						duration = "10m"
+					}
+                }
+				budget {
+                	reasons = ["Drift","Empty"]
+                    nodes = "1"
+					schedule {
+						frequency = "@monthly"
+						duration = "10m"
+					}
+                }
+			}
+		}
+		node_preferences {
+			ttl = "1h"
+            reverse_fallback_interval = "6h"
+		} 
+		commitment_integration {
+			enabled           = true
+			master_account_id = "dummyAccountId"
+		}
+		replacement_schedule {
+			window_type = "Custom"
+			applies_to {
+			  consolidation        = true
+			  harness_pod_eviction = true
+			  reverse_fallback     = true
+			}
+			window_details {
+			  days       = ["SUN", "WED", "SAT"]
+			  time_zone  = "Asia/Calcutta"
+			  all_day    = false
+			  start_time = "10:30"
+			  end_time   = "11:30"
+			}
+		}
+	}
+`, orchID, disabled)
 }
